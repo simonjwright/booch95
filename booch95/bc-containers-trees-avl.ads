85c5b2e401ccfd9f92f8659ad96a5fffcfc3c0b3
Simon Wright <simon@pushface.org>
1998-07-16 04:59:02 +0000

    Replace original copyright with RCS Id. Indent, case, and parens neatened.

--- booch95/bc-containers-trees-avl.ads
+++ booch95/bc-containers-trees-avl.ads
@@ -1,48 +1,46 @@
---  The C++ Booch Components (Version 2.3)
---  (C) Copyright 1990-1994 Grady Booch. All Rights Reserved.
---
---  BCATree.h
---
---  This file contains the declaration of the AVL tree.
+-- $Id$
 
 with Bc.Support.Nodes;
 generic
 package BC.Containers.Trees.AVL is
 
-   type AVL_Tree is limited private;
+  type AVL_Tree is limited private;
 
-   type Is_Less_Than is access function(L,R : in Item) return Boolean;
-   type Iteration_Function is access function(Elem: Item) return Boolean;
+  type Is_Less_Than is access function (L,R : in Item) return Boolean;
+  type Iteration_Function is access function (Elem: Item) return Boolean;
 
-   type Child_Branch is (Left, Right);
+  type Child_Branch is (Left, Right);
 
-   procedure Set_Less_Than_Func(Obj : in out AVL_Tree; F : Is_Less_Than);
+  procedure Set_Less_Than_Func (Obj : in out AVL_Tree; F : Is_Less_Than);
 
-   procedure Clear(Obj : in out AVL_Tree);
-   procedure Insert(Obj : in out AVL_Tree; Elem : in Item;
-					   Already_Existed : out Boolean);
-   procedure Delete(obj : in out AVL_Tree; Elem : Item; Found : out Boolean);
+  procedure Clear (Obj : in out AVL_Tree);
+  procedure Insert (Obj : in out AVL_Tree;
+                    Elem : in Item;
+                    Already_Existed : out Boolean);
+  procedure Delete (Obj : in out AVL_Tree; Elem : Item; Found : out Boolean);
 
-   function Extent(Obj : in AVL_Tree) return Natural;
+  function Extent (Obj : in AVL_Tree) return Natural;
 
-   function Is_Null(Obj : in AVL_Tree) return boolean;
-   function Is_Member(Obj : in AVL_Tree; Elem : Item) return boolean;
+  function Is_Null (Obj : in AVL_Tree) return Boolean;
+  function Is_Member (Obj : in AVL_Tree; Elem : Item) return Boolean;
 
-   function Item_Of(Obj : AVL_Tree; Elem : Item ) return Item_Ptr;
+  function Item_Of (Obj : AVL_Tree; Elem : Item ) return Item_Ptr;
 
-   function Traverse(Obj : in AVL_Tree; Iter_Func : Iteration_Function)
-     return Boolean;
+  function Traverse (Obj : in AVL_Tree; Iter_Func : Iteration_Function)
+                     return Boolean;
 
 private
-   package Nodes is new Bc.Support.Nodes(Item); use Nodes;
 
-   type AVL_Tree is new Limited_Controlled with record
-      Rep : AVL_Node_Ref;
-      Size : Natural := 0;
-      Less_Than : Is_Less_Than;
-   end record;
+  package Nodes is new Bc.Support.Nodes (Item);
+  use Nodes;
 
-   procedure Initialize(Obj : in out AVL_Tree);
-   procedure Finalize(Obj : in out AVL_Tree);
+  type AVL_Tree is new Limited_Controlled with record
+    Rep : AVL_Node_Ref;
+    Size : Natural := 0;
+    Less_Than : Is_Less_Than;
+  end record;
+
+  procedure Initialize (Obj : in out AVL_Tree);
+  procedure Finalize (Obj : in out AVL_Tree);
 
 end BC.Containers.Trees.AVL;
