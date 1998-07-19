2a837c754866caa042dbdf97abc0ba0f8ad067ad
simon <simon@localhost>
1998-07-19 19:37:25 +0000

    Added copyright. Noted that list forms /are/ derived from Container.

--- booch95/bc-containers-lists.ads
+++ booch95/bc-containers-lists.ads
@@ -1,2 +1,23 @@
---NOTE: List forms are NOT derived from type Container!
+-- Copyright (C) 1994-1998 Grady Booch, David Weller and Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+-- XXX This note doesn't seem to be true.
+-- NOTE: List forms are NOT derived from type Container!
+
 generic package Bc.Containers.Lists is end;
