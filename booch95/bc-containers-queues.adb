e5ea4f00e5b48ff0d4ab12659dddf71ed5248569
Simon Wright <simon@pushface.org>
1998-07-19 19:57:19 +0000

    New copyright. Format cleanup.

--- booch95/bc-containers-queues.adb
+++ booch95/bc-containers-queues.adb
@@ -1,5 +1,5 @@
--- The Ada 95 Booch Components (Version 1.0 beta 1)
--- Copyright (C)1994-1997 Grady Booch and David Weller.  All Rights Reserved.
+-- Copyright (C) 1994-1998 Grady Booch, David Weller and Simon Wright.
+-- All Rights Reserved.
 --
 --      This program is free software; you can redistribute it
 --      and/or modify it under the terms of the Ada Community
@@ -14,47 +14,45 @@
 --      License" or "ACL". If not, contact the author of this library
 --      for a copy.
 --
---  This file contains the implementation of the queue abstract base class
---  (The non-abstract parts, that is)
+
+-- $Id$
 
 package body Bc.Containers.Queues is
 
-   function "=" (Left, Right : access Queue'Class) return Boolean is
-   begin
-      if Left.all = Right.all then
-         return True;
-      elsif Cardinality (Left.all) /= Cardinality (Right.all) then
-         return False;
-      else
-         declare
-            Left_Iter : Iterator (Left);
-            Right_Iter : Iterator (Right);
-         begin
-            while not Is_Done (Left_Iter) and then
-              not Is_Done (Right_Iter) loop
-               if Current_Item (Left_Iter).all /=
-                 Current_Item (Right_Iter).all then
-                  return False;
-               end if;
-               Next (Left_Iter);
-               Next (Right_Iter);
-            end loop;
-            return True;
-         end;
-      end if;
-   end "=";
+  function "=" (Left, Right : access Queue'Class) return Boolean is
+  begin
+    if Left.all = Right.all then
+      return True;
+    elsif Cardinality (Left.all) /= Cardinality (Right.all) then
+      return False;
+    else
+      declare
+        Left_Iter : Iterator (Left);
+        Right_Iter : Iterator (Right);
+      begin
+        while not Is_Done (Left_Iter) and then
+           not Is_Done (Right_Iter) loop
+          if Current_Item (Left_Iter).all /=
+             Current_Item (Right_Iter).all then
+            return False;
+          end if;
+          Next (Left_Iter);
+          Next (Right_Iter);
+        end loop;
+        return True;
+      end;
+    end if;
+  end "=";
 
-   procedure Copy (From : access Queue'Class; To : access Queue'Class) is
-      Iter : Iterator (From);
-   begin
-      Clear (To.all);
-      Reset (Iter);
-      while not Is_Done (Iter) loop
-         Append (To.all, Current_Item (Iter).all);
-         Next (Iter);
-      end loop;
-   end Copy;
+  procedure Copy (From : access Queue'Class; To : access Queue'Class) is
+    Iter : Iterator (From);
+  begin
+    Clear (To.all);
+    Reset (Iter);
+    while not Is_Done (Iter) loop
+      Append (To.all, Current_Item (Iter).all);
+      Next (Iter);
+    end loop;
+  end Copy;
 
 end Bc.Containers.Queues;
-
-
--- booch95/bc-containers-queues.ads
+++ booch95/bc-containers-queues.ads
@@ -1,5 +1,5 @@
--- The Ada 95 Booch Components (Version 1.0 beta 1)
--- Copyright (C)1994-1997 Grady Booch and David Weller.  All Rights Reserved.
+-- Copyright (C) 1994-1998 Grady Booch, David Weller and Simon Wright.
+-- All Rights Reserved.
 --
 --      This program is free software; you can redistribute it
 --      and/or modify it under the terms of the Ada Community
@@ -14,62 +14,63 @@
 --      License" or "ACL". If not, contact the author of this library
 --      for a copy.
 --
--- This file contains the specification of the queue abstract base class
+
+-- $Id$
 
 generic
 package Bc.Containers.Queues is
 
-   type Queue is abstract new Container with private;
+  type Queue is abstract new Container with private;
 
-   -- A sequence in which items may be added from one end and removed from
-   -- the opposite end.  This class is abstract and serves only to enforce
-   -- the interfaces among classes.
+  -- A sequence in which items may be added from one end and removed from
+  -- the opposite end.  This class is abstract and serves only to enforce
+  -- the interfaces among classes.
 
-   -- Operations of equality, inequality, and assignment are "deep" for
-   -- all Queue forms
+  -- Operations of equality, inequality, and assignment are "deep" for
+  -- all Queue forms
 
-   procedure Clear   (Obj : in out Queue) is abstract;
-   -- Empty the Queue of all items
+  procedure Clear   (Obj : in out Queue) is abstract;
+  -- Empty the Queue of all items
 
-   procedure Append  (Obj : in out Queue; Elem : Item) is abstract;
-   -- Add the item to the back of the Queue; the Item itself is copied
+  procedure Append  (Obj : in out Queue; Elem : Item) is abstract;
+  -- Add the item to the back of the Queue; the Item itself is copied
 
-   procedure Pop     (Obj : in out Queue) is abstract;
-   -- Remove the Item from the front of the Queue
+  procedure Pop     (Obj : in out Queue) is abstract;
+  -- Remove the Item from the front of the Queue
 
-   procedure Remove  (Obj : in out Queue; From : Natural) is abstract;
-   -- Remove the Item at the given Index (balking operation)
+  procedure Remove  (Obj : in out Queue; From : Natural) is abstract;
+  -- Remove the Item at the given Index (balking operation)
 
-   function Length   (Obj : in Queue) return Natural is abstract;
-   -- Returns total items in the Queue
+  function Length   (Obj : in Queue) return Natural is abstract;
+  -- Returns total items in the Queue
 
-   function Is_Empty (Obj : in Queue) return Boolean is abstract;
-   -- Returns true iff no items are in the queue
+  function Is_Empty (Obj : in Queue) return Boolean is abstract;
+  -- Returns true iff no items are in the queue
 
-   function Front    (Obj : in Queue) return Item is abstract;
-   -- Return the item at the front of the Queue; the Item is _not_ removed
+  function Front    (Obj : in Queue) return Item is abstract;
+  -- Return the item at the front of the Queue; the Item is _not_ removed
 
-   function Front    (Obj : in Queue) return Item_Ptr is abstract;
-   -- Return reference to item at the front of the Queue;
-   -- the Item is _not_ removed
+  function Front    (Obj : in Queue) return Item_Ptr is abstract;
+  -- Return reference to item at the front of the Queue;
+  -- the Item is _not_ removed
 
-   function Location (Obj : in Queue; Elem : in Item) return Natural
-      is abstract;
-   -- Returns the Index number where the Item is found.  A zero is
-   -- returned on failure
+  function Location (Obj : in Queue; Elem : in Item) return Natural
+    is abstract;
+  -- Returns the Index number where the Item is found.  A zero is
+  -- returned on failure
 
-   function "=" (Left, Right : access Queue'Class) return Boolean;
+  function "=" (Left, Right : access Queue'Class) return Boolean;
 
-   procedure Copy (From : access Queue'Class; To : access Queue'Class);
-   -- This operation MUST be called for dissimilar Queues in place of
-   -- assignment.
+  procedure Copy (From : access Queue'Class; To : access Queue'Class);
+  -- This operation MUST be called for dissimilar Queues in place of
+  -- assignment.
 
 private
 
-   type Queue is abstract new Container with null record;
+  type Queue is abstract new Container with null record;
 
-   procedure Purge (Obj : in out Queue) is abstract;
-   procedure Add (Obj : in out Queue; Elem : in out Item) is abstract;
+  procedure Purge (Obj : in out Queue) is abstract;
+  procedure Add (Obj : in out Queue; Elem : in out Item) is abstract;
 
 end Bc.Containers.Queues;
 
