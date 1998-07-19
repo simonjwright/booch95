94eb78484d50fcf540f74df6e37f9455dcf10aed
Simon Wright <simon@pushface.org>
1998-07-19 19:28:09 +0000

    Format cleanup. All indices now Natural.

--- booch95/bc-containers.adb
+++ booch95/bc-containers.adb
@@ -1,6 +1,6 @@
--- The Ada 95 Booch Components (Version 1.0 beta 1)
--- Copyright (C)1994-1997 Grady Booch and David Weller.  All Rights Reserved.
--- 
+-- Copyright (C) 1994-1998 Grady Booch, David Weller and Simon Wright.
+-- All Rights Reserved.
+--
 --      This program is free software; you can redistribute it
 --      and/or modify it under the terms of the Ada Community
 --      License which comes with this Library.
@@ -11,74 +11,75 @@
 --      PURPOSE. See the Ada Community License for more details.
 --      You should have received a copy of the Ada Community
 --      License with this library, in the file named "Ada Community
---      License" or "ACL". If not, contact the author of this library 
+--      License" or "ACL". If not, contact the author of this library
 --      for a copy.
 --
--- This File Contains the implementation of the abstract container class
 
-package body BC.Containers is 
+-- $Id$
 
-   procedure Reset (Obj : in out Iterator) is
-   begin
-      if Cardinality (Obj.C.all) > 0 then
-     Obj.Index := 1;
+package body BC.Containers is
+
+  procedure Reset (Obj : in out Iterator) is
+  begin
+    if Cardinality (Obj.C.all) > 0 then
+      Obj.Index := 0;
+    else
+      Obj.Index := -1;
+    end if;
+  end Reset;
+
+  procedure Next (Obj : in out Iterator) is
+  begin
+    Obj.Index := Obj.Index + 1;
+  end Next;
+
+  function Is_Done (Obj : Iterator) return Boolean is
+  begin
+    return (Obj.Index < 0) or else (Obj.Index > Cardinality (Obj.C.all));
+  end Is_Done;
+
+  function Current_Item (Obj : Iterator) return Item is
+  begin
+    return Item_At (Obj.C.all, Obj.Index).all;
+  end Current_Item;
+
+  function Current_Item (Obj : Iterator) return Item_Ptr is
+  begin
+    return Item_At (Obj.C.all, Obj.Index);
+  end Current_Item;
+
+  function Visit (Obj : access Passive_Iterator) return Boolean is
+    Iter : Iterator (Obj.C);
+    Temp : Item;
+    Success : Boolean;
+  begin
+    while not Is_Done (Iter) loop
+      Temp := Current_Item (Iter);
+      Apply (Temp, Success);
+      if Success then
+        Next (Iter);
       else
-     Obj.Index := 0;
+        return False;
       end if;
-   end Reset;
+    end loop;
+    return True;
+  end Visit;
 
-   procedure Next (Obj : in out Iterator) is
-   begin
-      Obj.Index := Obj.Index + 1;
-   end Next;
-
-   function Is_Done (Obj : Iterator) return Boolean is
-   begin
-     return (Obj.Index < 1) or else (Obj.Index > Cardinality (Obj.C.all));
-   end Is_Done;
-
-   function Current_Item (Obj : Iterator) return Item is
-   begin
-      return Item_At (Obj.C.all, Obj.Index).all;
-   end Current_Item;
-
-   function Current_Item (Obj : Iterator) return Item_Ptr is
-   begin
-      return Item_At (Obj.C.all, Obj.Index);
-   end Current_Item;
-
-   function Visit (Obj : access Passive_Iterator) return Boolean is
-      Iter : Iterator(Obj.C);
-      Temp : Item;
-      Success : Boolean;
-   begin
-      while not Is_Done(Iter) loop
-	 Temp := Current_Item(Iter);
-	 Apply(Temp, Success);
-	 if Success then
-	    Next(Iter);
-	 else
-	    return False;
-	 end if;
-      end loop;
-      return True;
-   end Visit;
-
-   function Modify (Obj : access Passive_Iterator) return Boolean is
-      Iter : Iterator(Obj.C);
-      Temp : Item_Ptr;
-      Success : Boolean;
-   begin
-      while not Is_Done(Iter) loop
-	 Temp := Current_Item(Iter);
-	 Apply(Temp, Success);
-	 if Success then
-	    Next(Iter);
-	 else
-	    return False;
-	 end if;
-      end loop;
-      return True;
-   end Modify;
+  function Modify (Obj : access Passive_Iterator) return Boolean is
+    Iter : Iterator (Obj.C);
+    Temp : Item_Ptr;
+    Success : Boolean;
+  begin
+    while not Is_Done (Iter) loop
+      Temp := Current_Item (Iter);
+      Apply (Temp, Success);
+      if Success then
+        Next (Iter);
+      else
+        return False;
+      end if;
+    end loop;
+    return True;
+  end Modify;
 
 end BC.Containers;
