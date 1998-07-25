d62294e19b3cee33d57fe475166b5c960ece1adc
Simon Wright <simon@pushface.org>
1998-07-25 16:53:54 +0000

    Reverted the tree to being just Controlled.

--- booch95/bc-containers-trees-multiway.ads
+++ booch95/bc-containers-trees-multiway.ads
@@ -1,3 +1,20 @@
+-- Copyright (C) 1994-1998 Grady Booch, David Weller and Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
 -- $Id$
 
 with BC.Support.Nodes;
@@ -5,9 +22,9 @@ with BC.Support.Nodes;
 generic
 package BC.Containers.Trees.Multiway is
 
-  type Multiway_Tree is limited private;
+  type Multiway_Tree is private;
 
-  --    function Create(From : Multiway_Tree) return Multiway_Tree;
+  function Create (From : Multiway_Tree) return Multiway_Tree;
 
   function "=" (Left, Right : Multiway_Tree) return Boolean;
 
@@ -43,14 +60,15 @@ package BC.Containers.Trees.Multiway is
 
 private
 
-  package Nodes is new Bc.Support.Nodes(Item);
+  package Nodes is new Bc.Support.Nodes (Item);
   use Nodes;
 
-  type Multiway_Tree is new Limited_Controlled with record
+  type Multiway_Tree is new Ada.Finalization.Controlled with record
     Rep : Multiway_Node_Ref;
   end record;
 
   procedure Initialize (Obj : in out Multiway_Tree);
+  procedure Adjust (Obj : in out Multiway_Tree);
   procedure Finalize (Obj : in out Multiway_Tree);
 
 end BC.Containers.Trees.Multiway;
