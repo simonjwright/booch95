fd284f78567b95804fe48a198540f118dbd2a665
simon <simon@localhost>
1998-09-17 05:27:27 +0000

    Added annotation. Moved 'protected' subprograms to private part.

--- booch95/bc-containers-queues-unbounded.ads
+++ booch95/bc-containers-queues-unbounded.ads
@@ -31,23 +31,38 @@ package BC.Containers.Queues.Unbounded is
   -- available memory.  Assignment is "deep".
 
   procedure Clear (Obj : in out Unb_Queue);
+  -- Empty the queue of all items.
+
   procedure Append (Obj : in out Unb_Queue; Elem : Item);
+  -- Add the item to the back of the queue; the item itself is copied.
+
   procedure Pop (Obj : in out Unb_Queue);
+  -- Remove the item from the front of the queue.
+
   procedure Remove (Obj : in out Unb_Queue; From : Natural);
+  -- Remove the item at the given index.
+
   function Length (Obj : in Unb_Queue) return Natural;
+  -- Return the number of items in the queue.
+
   function Is_Empty (Obj : in Unb_Queue) return Boolean;
+  -- Return True if and only if there are no items in the queue.
+
   function Front (Obj : in Unb_Queue) return Item;
+  -- Return a copy of the item at the front of the queue.
+
   function Front (Obj : in Unb_Queue) return Item_Ptr;
+  -- Return a pointer to the item at the front of the queue.
+
   function Location (Obj : in Unb_Queue; Elem : Item) return Natural;
+  -- Return the first index at which the item is found; return 0 if the
+  -- item does not exist in the queue.
 
   function "=" (Left, Right : in Unb_Queue) return Boolean;
-  function Cardinality (Obj : in Unb_Queue) return Integer;
-  procedure Purge (Obj : in out Unb_Queue);
-  procedure Add (Obj : in out Unb_Queue; Elem : in out Item);
+  -- Return True if and only if both queues have the same length and the same
+  -- items in the same order; return False otherwise.
 
 private
-
-  function Item_At (Obj : in Unb_Queue; Index : in Natural) return Item_Ptr;
 
   package Unb_Queue_Nodes
   is new BC.Support.Unbounded (Item, Item_Ptr, Storage_Manager, Storage);
@@ -55,6 +70,11 @@ private
   type Unb_Queue is new Queue with record
     Rep : Unb_Queue_Nodes.Unb_Node_Ref := new Unb_Queue_Nodes.Unb_Node;
   end record;
+
+  function Item_At (Obj : in Unb_Queue; Index : in Natural) return Item_Ptr;
+  function Cardinality (Obj : in Unb_Queue) return Integer;
+  procedure Purge (Obj : in out Unb_Queue);
+  procedure Add (Obj : in out Unb_Queue; Elem : in out Item);
 
   procedure Initialize (Obj : in out Unb_Queue);
   procedure Adjust (Obj : in out Unb_Queue);
