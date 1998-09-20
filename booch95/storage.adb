d9a8c4b06db952d4cb7b2847740bed2c5558a6ae
simon <simon@localhost>
1998-09-20 15:06:16 +0000

    Removed unmanaged versions. Include Pat's new Managed_Storage.Pool_Overhead().

--- booch95/storage.adb
+++ booch95/storage.adb
@@ -2,41 +2,39 @@ With Ada.Text_Io;
 with Ada.Integer_Text_Io;
 with Ada.Unchecked_Deallocation;
 with BC.Support.Managed_Storage;
-with BC.Support.Unmanaged_Storage;
+
+with System.Storage_Elements;
+
 
 procedure Storage is
 
-  Unmanaged_Pool : BC.Support.Unmanaged_Storage.Pool;
-  Managed_Pool : BC.Support.Managed_Storage.Pool (4096);
+  use type System.Storage_Elements.Storage_Count;
+
+  package Managed_Storage renames BC.Support.Managed_Storage;
 
   type T is array (Integer range <>) of Character;
-  type U_P is access T;
-  for U_P'Storage_Pool use Unmanaged_Pool;
+
+  T_Overhead : constant := 2 * (System.Word_Size / System.Storage_Unit);
+    -- to include dope for lower/upper bounds, since T is unconstrained
+
+  Managed_Pool : Managed_Storage.Pool
+     (4096 + Managed_Storage.Pool_Overhead (T_Overhead, T'Alignment));
+
   type M_P is access T;
   for M_P'Storage_Pool use Managed_Pool;
 
-  procedure Delete is new Ada.Unchecked_Deallocation (T, U_P);
   procedure Delete is new Ada.Unchecked_Deallocation (T, M_P);
 
-  U_Ptr : U_P;
   M_Ptr : M_P;
 
   Trying : Integer;
 
 begin
 
-  Ada.Text_Io.Put_Line ("trying unmanaged store:");
-  for I in 0 .. 4096 loop
-    Trying := I;
-    U_Ptr := new T (1 .. Trying);
-    Delete (U_Ptr);
-  end loop;
-  Ada.Text_Io.Put_Line (".. done.");
-
   Ada.Text_Io.Put_Line ("trying managed store:");
-    for I in 0 .. 4096 loop
+  for I in 0 .. 4097 loop
     Trying := I;
-    M_Ptr := new T (1 .. Trying);
+    M_Ptr := new T(1 .. Trying);
     Delete (M_Ptr);
   end loop;
   Ada.Text_Io.Put_Line (".. done.");
@@ -47,3 +45,4 @@ exception
     Ada.Integer_Text_Io.Put (Trying);
     Ada.Text_Io.New_Line;
 end Storage;
+
