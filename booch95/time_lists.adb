820d28e4f5873942c2fbdd2dfd0359a39799ac55
simon <simon@localhost>
1998-09-20 14:59:24 +0000

    First version.

---
+++ booch95/time_lists.adb
@@ -0,0 +1,66 @@
+with Ada.Calendar;
+with Ada.Text_Io;
+with Lists_For_Timing;
+procedure Time_Lists is
+  S : aliased Lists_For_Timing.S.Single_List;
+  D : aliased Lists_For_Timing.D.Double_List;
+  S_Iter : aliased Lists_For_Timing.C.Passive_Iterator (S'Access);
+  D_Iter : aliased Lists_For_Timing.C.Passive_Iterator (D'Access);
+  procedure Iterate is
+    Total : Integer;
+    procedure Apply (Elem : Integer; Ok : out Boolean) is
+    begin
+      Total := Total + Elem;
+      Ok := True;
+    end Apply;
+    function L_Application is new Lists_For_Timing.C.Visit (Apply);
+    Result : Boolean;
+    Start : Ada.Calendar.Time;
+    Taken : Duration;
+    use type Ada.Calendar.Time;
+  begin
+
+    Total := 0;
+    Start := Ada.Calendar.Clock;
+    Result := L_Application (S_Iter'Access);
+    Taken := Ada.Calendar.Clock - Start;
+    Ada.Text_Io.Put_Line
+       (".. single list took" & Taken'Img & " sec, sum" & Total'Img);
+
+    Total := 0;
+    Start := Ada.Calendar.Clock;
+    Result := L_Application (D_Iter'Access);
+    Taken := Ada.Calendar.Clock - Start;
+    Ada.Text_Io.Put_Line
+       (".. double list took" & Taken'Img & " sec, sum" & Total'Img);
+
+  end Iterate;
+  procedure Time (N : Integer) is
+  begin
+    Ada.Text_Io.Put_Line
+       ("timing iteration over containers of length" & N'Img);
+    Lists_For_Timing.S.Clear (S);
+    Lists_For_Timing.D.Clear (D);
+    for I in 1 .. N loop
+      Lists_For_Timing.S.Append (S, I);
+      Lists_For_Timing.D.Append (D, I);
+    end loop;
+    Iterate;
+  end Time;
+begin
+  Time (1);
+  Time (2);
+  Time (4);
+  Time (8);
+  Time (16);
+  Time (32);
+  Time (64);
+  Time (128);
+  Time (256);
+  Time (512);
+  Time (1024);
+  Time (2048);
+exception
+  when others =>
+    Ada.Text_Io.Put_Line ("oops");
+end Time_Lists;
