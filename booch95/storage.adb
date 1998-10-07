c1cd052a6052407c43b96f8e4f76efc718762ee1
simon <simon@localhost>
1998-10-07 05:09:57 +0000

    Added trial of Unmanaged_Store. Output some annotation.

--- booch95/storage.adb
+++ booch95/storage.adb
@@ -17,10 +17,11 @@
 
 -- $Id$
 
-With Ada.Text_Io;
+with Ada.Text_Io;
 with Ada.Integer_Text_Io;
 with Ada.Unchecked_Deallocation;
 with BC.Support.Managed_Storage;
+with BC.Support.Unmanaged_Storage;
 
 with System.Storage_Elements;
 
@@ -30,6 +31,7 @@ procedure Storage is
   use type System.Storage_Elements.Storage_Count;
 
   package Managed_Storage renames BC.Support.Managed_Storage;
+  package Unmanaged_Storage renames BC.Support.Unmanaged_Storage;
 
   type T is array (Integer range <>) of Character;
 
@@ -38,30 +40,59 @@ procedure Storage is
 
   Managed_Pool : Managed_Storage.Pool
      (4096 + Managed_Storage.Pool_Overhead (T_Overhead, T'Alignment));
+  Unmanaged_Pool : Unmanaged_Storage.Pool;
 
   type M_P is access T;
-  for M_P'Storage_Pool use Managed_Pool;
+  for M_P'Storage_pool use Managed_Pool;
+  type U_P is access T;
+  for U_P'Storage_pool use Unmanaged_Pool;
 
   procedure Delete is new Ada.Unchecked_Deallocation (T, M_P);
+  procedure Delete is new Ada.Unchecked_Deallocation (T, U_P);
 
   M_Ptr : M_P;
+  U_Ptr : U_P;
 
   Trying : Integer;
 
 begin
 
-  Ada.Text_Io.Put_Line ("trying managed store:");
-  for I in 0 .. 4097 loop
-    Trying := I;
-    M_Ptr := new T(1 .. Trying);
-    Delete (M_Ptr);
-  end loop;
-  Ada.Text_Io.Put_Line (".. done.");
+  begin
+    Ada.Text_Io.Put_Line ("Trying Managed Store.");
+    Ada.Text_Io.Put_Line ("We expect this to fail at size 4097.");
+    Ada.Text_Io.Put_Line ("Note, this allocation pattern is" &
+                          " pathologically bad for Managed Store!");
+    for I in 0 .. 4097 loop
+      Trying := I;
+      M_Ptr := new T(1 .. Trying);
+      Delete (M_Ptr);
+    end loop;
+    Ada.Text_Io.Put_Line (".. done all 4097 allocations.");
 
-exception
-  when BC.Storage_Error =>
-    Ada.Text_Io.Put ("exception BC.Storage_Error raised at size");
-    Ada.Integer_Text_Io.Put (Trying);
-    Ada.Text_Io.New_Line;
+  exception
+    when BC.Storage_Error =>
+      Ada.Text_Io.Put ("exception BC.Storage_Error raised at size");
+      Ada.Integer_Text_Io.Put (Trying);
+      Ada.Text_Io.New_Line;
+
+  end;
+
+  begin
+    Ada.Text_Io.Put_Line ("Trying Unmanaged Store:");
+    for I in 0 .. 4097 loop
+      Trying := I;
+      U_Ptr := new T(1 .. Trying);
+      Delete (U_Ptr);
+    end loop;
+    Ada.Text_Io.Put_Line (".. done all 4097 allocations.");
+
+  exception
+    when BC.Storage_Error =>
+      Ada.Text_Io.Put ("exception BC.Storage_Error raised at size");
+      Ada.Integer_Text_Io.Put (Trying);
+      Ada.Text_Io.New_Line;
+
+  end;
+
 end Storage;
 
