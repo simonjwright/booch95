6a04fee337696566e181e156583bc154da8bb823
simon <simon@localhost>
1998-10-07 05:20:27 +0000

    Added copyright.

--- booch95/lists_for_timing.ads
+++ booch95/lists_for_timing.ads
@@ -1,3 +1,22 @@
+-- Copyright (C) 1998 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
 with BC.Containers;
 with BC.Containers.Lists;
 with BC.Containers.Lists.Single;
@@ -8,7 +27,7 @@ package Lists_For_Timing is
   type Integer_P is access all Integer;
   package C is new BC.Containers (Integer, Integer_P);
   package L is new C.Lists;
-  P : BC.Support.Managed_Storage.Pool (16#100_000#);
+  P : BC.Support.Managed_Storage.Pool (10_000);
   package S is new L.Single (BC.Support.Managed_Storage.Pool, P);
   package D is new L.Double (BC.Support.Managed_Storage.Pool, P);
 --   P : BC.Support.Unmanaged_Storage.Pool;
--- booch95/queues_for_timing.ads
+++ booch95/queues_for_timing.ads
@@ -7,6 +7,25 @@ with BC.Support.Bounded;
 with BC.Support.Dynamic;
 with BC.Support.Managed_Storage;
 with BC.Support.Unmanaged_Storage;
+-- Copyright (C) 1998 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
 package Queues_For_Timing is
   Size : constant := 10_000;
   type Integer_P is access all Integer;
