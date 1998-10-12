c1f6b3570725c1dd7a4bd915a893bd788a3df0ee
simon <simon@localhost>
1998-10-12 20:45:41 +0000

    Added subprograms specific to this concrete form.

--- booch95/bc-graphs-directed.ads
+++ booch95/bc-graphs-directed.ads
@@ -1,4 +1,4 @@
--- Copyright (C) 1994-1998 Grady Booch, David Weller and Simon Wright.
+-- Copyright (C) 1994-1998 Grady Booch and Simon Wright.
 -- All Rights Reserved.
 --
 --      This program is free software; you can redistribute it
@@ -23,6 +23,36 @@ package BC.Graphs.Directed is
   type Graph is new Graphs.Graph with private;
   type Vertex is new Graphs.Vertex with private;
   type Arc is new Graphs.Arc with private;
+
+  ----------------------
+  -- Graph operations --
+  ----------------------
+
+  procedure Create_Arc (G : in out Graph;
+                        A : in out Arc'Class;
+                        I : Arc_Item;
+                        From : in out Vertex'Class;
+                        To : in out Vertex'Class);
+
+  -----------------------
+  -- Vertex operations --
+  -----------------------
+
+  function Number_Of_Incoming_Arcs (V : Vertex) return Natural;
+
+  function Number_Of_Outgoing_Arcs (V : Vertex) return Natural;
+
+  --------------------
+  -- Arc operations --
+  --------------------
+
+  procedure Set_From_Vertex (A : in out Arc; V : access Vertex'Class);
+
+  procedure Set_To_Vertex (A : in out Arc; V : access Vertex'Class);
+
+  procedure From_Vertex (A : Arc; V : in out Vertex'Class);
+
+  procedure To_Vertex (A : Arc; V : in out Vertex'Class);
 
 private
 
