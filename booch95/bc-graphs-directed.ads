5f3b77f240f88d94cba436c712a6b9f2a5e3470f
Simon Wright <simon@pushface.org>
1998-10-14 20:43:58 +0000

    Added Iterators.

--- booch95/bc-graphs-directed.ads
+++ booch95/bc-graphs-directed.ads
@@ -54,10 +54,54 @@ package BC.Graphs.Directed is
 
   procedure To_Vertex (A : Arc; V : in out Vertex'Class);
 
+  ---------------------
+  -- Graph iterators --
+  ---------------------
+
+  type Passive_Graph_Iterator (G : access Graph) is limited private;
+
+  generic
+    with procedure Apply (V : Vertex; OK : out Boolean);
+  function Visit_Vertices (It : access Passive_Graph_Iterator) return Boolean;
+
+  ---------------------
+  -- Vertex iterators --
+  ---------------------
+
+  type Vertex_Iterator (V : access Vertex'Class) is limited private;
+
+  procedure Reset (It : in out Vertex_Iterator);
+
+  procedure Next (It : in out Vertex_Iterator);
+
+  function Is_Done (It : Vertex_Iterator) return Boolean;
+
+  procedure Current_Item (It : Vertex_Iterator; A : in out Arc'Class);
+
+  type Passive_Vertex_Iterator (V : access Vertex) is limited private;
+
+  generic
+    with procedure Apply (A : Arc; OK : out Boolean);
+  function Visit_Arcs (It : access Passive_Vertex_Iterator) return Boolean;
+
 private
 
   type Graph is new Graphs.Graph with null record;
   type Vertex is new Graphs.Vertex with null record;
   type Arc is new Graphs.Arc with null record;
+
+  type Passive_Graph_Iterator (G : access Graph) is limited record
+    Success : Boolean := False;
+  end record;
+
+  type Vertex_Iterator (V : access Vertex'Class)
+  is new Ada.Finalization.Limited_Controlled with record
+    Index : Arc_Node_Ptr;
+  end record;
+  procedure Initialize (It : in out Vertex_Iterator);
+
+  type Passive_Vertex_Iterator (V : access Vertex) is limited record
+    Success : Boolean := False;
+  end record;
 
 end BC.Graphs.Directed;
