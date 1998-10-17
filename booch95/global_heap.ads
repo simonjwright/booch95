638526148690de4caab7fbb65f992f081ef814f1
Simon Wright <simon@pushface.org>
1998-10-17 17:48:45 +0000

    Global_Heap now exports its own (subtype) Pool.

--- booch95/global_heap.ads
+++ booch95/global_heap.ads
@@ -21,6 +21,7 @@ with BC.Support.Managed_Storage;
 
 package Global_Heap is
 
-  Storage : BC.Support.Managed_Storage.Pool( Chunk_Size => 1024 );
+  subtype Pool is BC.Support.Managed_Storage.Pool;
+  Storage : Pool (Chunk_Size => 1024);
 
 end Global_Heap;
