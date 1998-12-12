c0bd9203b4b755776a9edcd9833eb3f8dca9f7a8
simon <simon@localhost>
1998-12-12 13:28:39 +0000

    Removed C++ code comments.

--- booch95/bc-containers-maps-bounded.adb
+++ booch95/bc-containers-maps-bounded.adb
@@ -21,23 +21,10 @@ with System.Address_To_Access_Conversions;
 
 package body BC.Containers.Maps.Bounded is
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| void BC_TBoundedMap<Item, Value, Buckets, Size>::Clear()
---| {
---|   fRep.Clear();
---| }
-
   procedure Clear (M : in out Bounded_Map) is
   begin
     Tables.Clear (M.Rep);
   end Clear;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::
---|   Bind(const Item& item, const Value& value)
---| {
---|   return fRep.Bind(item, value);
---| }
 
   procedure Bind
      (M : in out Bounded_Map; I : Item; V : Value) is
@@ -45,38 +32,16 @@ package body BC.Containers.Maps.Bounded is
     Tables.Bind (M.Rep, I, V);
   end Bind;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::
---|   Rebind(const Item& item, const Value& value)
---| {
---|   return fRep.Rebind(item, value);
---| }
-
   procedure Rebind
      (M : in out Bounded_Map; I : Item; V : Value) is
   begin
     Tables.Rebind (M.Rep, I, V);
   end Rebind;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::Unbind(const Item& item)
---| {
---|   return fRep.Unbind(item);
---| }
-
   procedure Unbind (M : in out Bounded_Map; I : Item) is
   begin
     Tables.Unbind (M.Rep, I);
   end Unbind;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Index BC_TBoundedMap<Item, Value, Buckets, Size>::Available() const
---| {
---|   BC_Index count = 0;
---|   for (BC_Index index = 0; (index < Buckets); index++)
---|     count += fRep.ItemBucket(index)->Available();
---|   return count;
---| }
 
   function Available (M : Bounded_Map) return Natural is
     Count : Natural := 0;
@@ -87,44 +52,20 @@ package body BC.Containers.Maps.Bounded is
     return Count;
   end Available;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Index BC_TBoundedMap<Item, Value, Buckets, Size>::Extent() const
---| {
---|   return fRep.Extent();
---| }
-
   function Extent (M : Bounded_Map) return Natural is
   begin
     return Tables.Extent (M.Rep);
   end Extent;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::IsEmpty() const
---| {
---|   return (fRep.Extent() == 0);
---| }
 
   function Is_Empty (M : Bounded_Map) return Boolean is
   begin
     return Tables.Extent (M.Rep) = 0;
   end Is_Empty;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::IsBound(const Item& item) const
---| {
---|   return fRep.IsBound(item);
---| }
-
   function Is_Bound (M : Bounded_Map; I : Item) return Boolean is
   begin
     return Tables.Is_Bound (M.Rep, I);
   end Is_Bound;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| const Value* BC_TBoundedMap<Item, Value, Buckets, Size>::ValueOf(const Item& item) const
---| {
---|   return fRep.ValueOf(item);
---| }
 
   function Value_Of (M : Bounded_Map; I : Item) return Value is
   begin
@@ -143,92 +84,41 @@ package body BC.Containers.Maps.Bounded is
 
   -- Private implementations
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| void BC_TBoundedMap<Item, Value, Buckets, Size>::Purge()
---| {
---|   fRep.Clear();
---| }
-
   procedure Purge (M : in out Bounded_Map) is
   begin
     Tables.Clear (M.Rep);
   end Purge;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::
---|   Attach(const Item& item, const Value& value)
---| {
---|   return fRep.Bind(item, value);
---| }
 
   procedure Attach (M : in out Bounded_Map; I : Item; V : Value) is
   begin
     Tables.Bind (M.Rep, I, V);
   end Attach;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Index BC_TBoundedMap<Item, Value, Buckets, Size>::Cardinality() const
---| {
---|   return fRep.Extent();
---| }
-
   function Cardinality (M : Bounded_Map) return Natural is
   begin
     return Tables.Extent (M.Rep);
   end Cardinality;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Index BC_TBoundedMap<Item, Value, Buckets, Size>::NumberOfBuckets() const
---| {
---|   return Buckets;
---| }
 
   function Number_Of_Buckets (M : Bounded_Map) return Natural is
   begin
     return Buckets;
   end Number_Of_Buckets;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Index BC_TBoundedMap<Item, Value, Buckets, Size>::Length(BC_Index bucket) const
---| {
---|   return fRep.ItemBucket(bucket)->Length();
---| }
-
   function Length (M : Bounded_Map; Bucket : Positive) return Natural is
   begin
     return IC.Length (Tables.Item_Bucket (M.Rep, Bucket).all);
   end Length;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| BC_Boolean BC_TBoundedMap<Item, Value, Buckets, Size>::Exists(const Item& item) const
---| {
---|   return fRep.IsBound(item);
---| }
 
   function Exists (M : Bounded_Map; I : Item) return Boolean is
   begin
     return Tables.Is_Bound (M.Rep, I);
   end Exists;
 
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| const Item& BC_TBoundedMap<Item, Value, Buckets, Size>::
---|   ItemAt(BC_Index bucket, BC_Index index) const
---| {
---|   return (*fRep.ItemBucket(bucket))[index];
---| }
-
   function Item_At
      (M : Bounded_Map; Bucket, Index : Positive) return Item_Ptr is
   begin
     return IC.Item_At (Tables.Item_Bucket (M.Rep, Bucket).all, Index);
   end Item_At;
-
---| template<class Item, class Value, BC_Index Buckets, BC_Index Size>
---| const Value& BC_TBoundedMap<Item, Value, Buckets, Size>::
---|   ValueAt(BC_Index bucket, BC_Index index) const
---| {
---|   return (*fRep.ValueBucket(bucket))[index];
---| }
 
   function Value_At
      (M : Bounded_Map; Bucket, Index : Positive) return Value_Ptr is
