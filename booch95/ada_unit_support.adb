3b25b340c91dd8b6ba5f4210f1ee0046a15f9166
Simon Wright <simon@pushface.org>
1999-01-04 06:50:52 +0000

    Updated for new Iterators.

--- booch95/ada_unit_support.adb
+++ booch95/ada_unit_support.adb
@@ -25,7 +25,7 @@ package body Ada_Unit_Support is
   package ASU renames Ada.Strings.Unbounded;
 
 
-  Info : aliased Dependencies.Graph;
+  Info : Dependencies.Directed_Graph;
 
 
   function Name (Of_The_Unit : Unit) return String is
@@ -33,26 +33,26 @@ package body Ada_Unit_Support is
     return ASU.To_String (Of_The_Unit.Named);
   end Name;
 
-  
+
   function Description (Of_The_Unit : Normal_Unit) return String is
   begin
     return "normal";
   end Description;
-  
+
 
   function Description (Of_The_Unit : Generic_Unit) return String is
   begin
     return "generic";
   end Description;
-  
+
 
   function Create_Normal_Unit (Unit_Named : String) return Unit_P is
     Result : Unit_P := new Normal_Unit;
   begin
     Result.Named := ASU.To_Unbounded_String (Unit_Named);
     Dependencies.Create_Vertex (G => Info,
-				V => Result.Vertex,
-				I => Result);
+                                V => Result.Vertex,
+                                I => Result);
     return Result;
   end Create_Normal_Unit;
 
@@ -62,74 +62,80 @@ package body Ada_Unit_Support is
   begin
     Result.Named := ASU.To_Unbounded_String (Unit_Named);
     Dependencies.Create_Vertex (G => Info,
-				V => Result.Vertex,
-				I => Result);
+                                V => Result.Vertex,
+                                I => Result);
     return Result;
   end Create_Generic_Unit;
 
 
   procedure Add_Dependency (Unit_Is_Withed : Unit_P; By : Unit_P) is
-    Arc : Dependencies.Arc;
+    Arc : Dependencies.Directed_Arc;
   begin
     Dependencies.Create_Arc (G => Info,
-			     A => Arc,
-			     I => (null record),
-			     From => By.Vertex,
-			     To => Unit_Is_Withed.Vertex);
+                             A => Arc,
+                             I => (null record),
+                             From => By.Vertex,
+                             To => Unit_Is_Withed.Vertex);
   end Add_Dependency;
-  
+
 
   procedure Report_Dependencies is
-    procedure Process_Dependency (A : Dependencies.Arc; OK : out Boolean) is
-      Withee : Dependencies.Vertex;
+    procedure Process_Dependency
+       (A : Dependencies_Base.Arc'Class; OK : out Boolean) is
+      Withee : Dependencies.Directed_Vertex;
     begin
-      Dependencies.To_Vertex (A, Withee);
+      Dependencies.To_Vertex (Dependencies.Directed_Arc (A), Withee);
       Ada.Text_Io.Put_Line ("  withs " & Name (Dependencies.Item (Withee).all));
       OK := True;
     end Process_Dependency;
-    procedure Process_Unit (V : Dependencies.Vertex; OK : out Boolean) is
-      Local_V : aliased Dependencies.Vertex := V;
-      Iter : aliased Dependencies.Passive_Vertex_Iterator (Local_V'Access);
-      function Visit
-      is new Dependencies.Visit_Arcs (Apply => Process_Dependency);
+    procedure Process_Unit
+       (V : Dependencies_Base.Vertex'Class; OK : out Boolean) is
+      procedure Visit
+      is new Dependencies_Base.Visit_Arcs (Apply => Process_Dependency);
+      Directed_V : Dependencies.Directed_Vertex
+         renames Dependencies.Directed_Vertex (V);
     begin
-      Ada.Text_Io.Put_Line (Description (Dependencies.Item (Local_V).all)
-			    & " unit "
-			    & Name (Dependencies.Item (Local_V).all));
-      OK := Visit (Iter'Access);
+      Ada.Text_Io.Put_Line (Description (Dependencies.Item (Directed_V).all)
+                            & " unit "
+                            & Name (Dependencies.Item (Directed_V).all));
+      Visit (Over_The_Vertex => V);
+      OK := True;
     end Process_Unit;
-    Iter : aliased Dependencies.Passive_Graph_Iterator (Info'Access);
-    function Visit is new Dependencies.Visit_Vertices (Apply => Process_Unit);
-    Dummy : Boolean;
+    procedure Visit is new Dependencies_Base.Visit_Vertices
+       (Apply => Process_Unit);
   begin
-    Dummy := Visit (Iter'Access);
+    Visit (Over_The_Graph => Info);
   end Report_Dependencies;
 
 
   procedure Report_Dependencies (For_Unit : Unit_P) is
     Indent : ASU.Unbounded_String;
-    procedure Process_Unit (V : Dependencies.Vertex; OK : out Boolean);
-    procedure Process_Dependency (A : Dependencies.Arc; OK : out Boolean) is
-      Withee : Dependencies.Vertex;
+    procedure Process_Unit
+       (V : Dependencies_Base.Vertex'Class; OK : out Boolean);
+    procedure Process_Dependency
+       (A : Dependencies_Base.Arc'Class; OK : out Boolean) is
+      Withee : Dependencies.Directed_Vertex;
     begin
-      Dependencies.To_Vertex (A, Withee);
+      Dependencies.To_Vertex (Dependencies.Directed_Arc (A), Withee);
       Process_Unit (Withee, OK);
     end Process_Dependency;
-    procedure Process_Unit (V : Dependencies.Vertex; OK : out Boolean) is
-      Local_V : aliased Dependencies.Vertex := V;
-      Iter : aliased Dependencies.Passive_Vertex_Iterator (Local_V'Access);
-      function Visit
-      is new Dependencies.Visit_Arcs (Apply => Process_Dependency);
+    procedure Process_Unit
+       (V : Dependencies_Base.Vertex'Class; OK : out Boolean) is
+      procedure Visit
+      is new Dependencies_Base.Visit_Arcs (Apply => Process_Dependency);
+      Directed_V : Dependencies.Directed_Vertex
+         renames Dependencies.Directed_Vertex (V);
       Old_Indent : ASU.Unbounded_String := Indent;
       use type ASU.Unbounded_String;
     begin
       Ada.Text_Io.Put_Line (ASU.To_String (Indent)
-			    & Description (Dependencies.Item (Local_V).all)
-			    & " unit "
-			    & Name (Dependencies.Item (Local_V).all));
+                            & Description (Dependencies.Item (Directed_V).all)
+                            & " unit "
+                            & Name (Dependencies.Item (Directed_V).all));
       Indent := Indent & "  ";
-      OK := Visit (Iter'Access);
+      Visit (V);
       Indent := Old_Indent;
+      OK := True;
     end Process_Unit;
     Dummy : Boolean;
   begin
