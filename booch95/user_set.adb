41040ac9d7b5ce98cab0c8db0b3eba0a0701d899
Simon Wright <simon@pushface.org>
1999-02-06 15:43:54 +0000

    First version.

---
+++ booch95/user_set.adb
@@ -0,0 +1,72 @@
+-- Copyright (C) 1999 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+with Ada.Text_Io;
+with User_Set_Support;
+
+procedure User_Set is
+
+  use Ada.Text_Io;
+  use User_Set_Support;
+
+  procedure Print_Set (S : in out Sets.Set'Class; Named : String) is
+    procedure Print (Item : User_Character; OK : out Boolean) is
+    begin
+      Put (" " & Character (Item));
+      OK := True;
+    end Print;
+    procedure Visitor is new Containers.Visit (Print);
+  begin
+    Put ("Set " & Named & " ");
+    Visitor (S);
+    New_Line;
+  end Print_Set;
+
+  procedure Test (S : in out Sets.Set'Class) is
+  begin
+    Sets.Add (S, 'a');
+    Sets.Add (S, 'B');
+    Sets.Add (S, '1');
+    Sets.Add (S, '2');
+    if not Sets.Is_Member (S, 'A') then
+      Sets.Add (S, 'A');
+    end if;
+    if not Sets.Is_Member (S, 'b') then
+      Sets.Add (S, 'b');
+    end if;
+    Print_Set (S, "S");
+    Sets.Remove (S, 'A');
+    Sets.Remove (S, 'b');
+    Print_Set (S, "S");
+  end Test;
+
+  B : SB.Bounded_Set;
+  D : SD.Dynamic_Set;
+  U : SU.Unbounded_Set;
+
+begin
+  Put_Line ("Hash of 'a' is " & Integer'Image (User_Char_Hash ('a')));
+  Put_Line ("Hash of 'A' is " & Integer'Image (User_Char_Hash ('A')));
+  Put_Line ("Bounded set:");
+  Test (B);
+  Put_Line ("Dynamic set:");
+  Test (D);
+  Put_Line ("Unbounded set:");
+  Test (U);
+end User_Set;
---
+++ booch95/user_set_support.adb
@@ -0,0 +1,37 @@
+-- Copyright (C) 1999 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+with Ada.Characters.Handling;
+
+package body User_Set_Support is
+
+  package ACH renames Ada.Characters.Handling;
+
+  function "=" (L, R : User_Character) return Boolean is
+  begin
+    return ACH.To_Upper (Character (L)) = ACH.To_Upper (Character (R));
+  end "=";
+
+  function User_Char_Hash (C : User_Character) return Positive is
+  begin
+    return Character'Pos (ACH.To_Upper (Character (C))) + 1;
+  end User_Char_Hash;
+
+end User_Set_Support;
+
