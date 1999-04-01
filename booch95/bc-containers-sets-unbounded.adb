e2918d9c8e3bb08e346b7c1e3fc2712636afa720
simon <simon@localhost>
1999-04-01 10:33:56 +0000

    In Remove, report exceptions raised locally (naming ourselves) rather than leaving it up to the supporting Hash Table.

--- booch95/bc-containers-sets-unbounded.adb
+++ booch95/bc-containers-sets-unbounded.adb
@@ -17,9 +17,14 @@
 
 -- $Id$
 
+with BC.Support.Exceptions;
 with System.Address_To_Access_Conversions;
 
 package body BC.Containers.Sets.Unbounded is
+
+  package BSE renames BC.Support.Exceptions;
+  procedure Assert
+  is new BSE.Assert ("BC.Containers.Sets.Unbounded");
 
   procedure Clear (S : in out Unbounded_Set) is
   begin
@@ -38,6 +43,10 @@ package body BC.Containers.Sets.Unbounded is
 
   procedure Remove (S : in out Unbounded_Set; I : Item) is
   begin
+    Assert (Tables.Is_Bound (S.Rep, I),
+            BC.Not_Found'Identity,
+            "Remove",
+            BSE.Missing);
     Tables.Unbind (S.Rep, I);
   end Remove;
 
