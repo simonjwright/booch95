3e1fd5b733f1f3e21bc7cdf49e6577ec404e03c5
Simon Wright <simon@pushface.org>
1999-04-12 21:08:50 +0000

    Removed procedure name comments, reordered to match body. Implemented null Lock, Unlock.

--- booch95/bc-containers-rings.adb
+++ booch95/bc-containers-rings.adb
@@ -19,28 +19,6 @@
 
 package body BC.Containers.Rings is
 
-  ---------
-  -- Add --
-  ---------
-
-  procedure Add (R : in out Ring; Elem : Item) is
-  begin
-    raise Should_Have_Been_Overridden;
-  end Add;
-
-  ------------
-  -- Adjust --
-  ------------
-
-  procedure Adjust (R : in out Ring) is
-  begin
-    null;
-  end Adjust;
-
-  ---------------
-  -- Are_Equal --
-  ---------------
-
   function Are_Equal (Left, Right : Ring'Class) return Boolean is
   begin
     -- XXX left out the optimisation which checks whether L, R are
@@ -64,19 +42,6 @@ package body BC.Containers.Rings is
     end;
   end Are_Equal;
 
-  -------------
-  -- At_Mark --
-  -------------
-
-  function At_Mark (R : Ring) return Boolean is
-  begin
-    return R.Mark = R.Top;
-  end At_Mark;
-
-  ----------
-  -- Copy --
-  ----------
-
   procedure Copy (From : Ring'Class; To : in out Ring'Class) is
     Iter : Iterator := New_Iterator (From);
   begin
@@ -97,9 +62,65 @@ package body BC.Containers.Rings is
     end if;
   end Copy;
 
-  ------------------
-  -- Current_Item --
-  ------------------
+  procedure Mark (R : in out Ring) is
+  begin
+    R.Mark := R.Top;
+  end Mark;
+
+  procedure Rotate_To_Mark (R : in out Ring) is
+  begin
+    R.Top := R.Mark;
+  end Rotate_To_Mark;
+
+  function At_Mark (R : Ring) return Boolean is
+  begin
+    return R.Mark = R.Top;
+  end At_Mark;
+
+  procedure Initialize (R : in out Ring) is
+  begin
+    R.Top := 0;
+    R.Mark := 0;
+  end Initialize;
+
+  procedure Add (R : in out Ring; Elem : Item) is
+  begin
+    raise Should_Have_Been_Overridden;
+  end Add;
+
+  procedure Lock (R : in out Ring) is
+  begin
+    null;
+  end Lock;
+
+  procedure Unlock (R : in out Ring) is
+  begin
+    null;
+  end Unlock;
+
+  procedure Initialize (It : in out Ring_Iterator) is
+  begin
+    Reset (It);
+  end Initialize;
+
+  procedure Reset (It : in out Ring_Iterator) is
+  begin
+    if Cardinality (It.R.all) = 0 then
+      It.Index := 0;
+    else
+      It.Index := 1;
+    end if;
+  end Reset;
+
+  function Is_Done (It : Ring_Iterator) return Boolean is
+  begin
+    return It.Index = 0 or else It.Index > Cardinality (It.R.all);
+  end Is_Done;
+
+  procedure Next (It : in out Ring_Iterator) is
+  begin
+    It.Index := It.Index + 1;
+  end Next;
 
   function Current_Item (It : Ring_Iterator) return Item is
   begin
@@ -118,10 +139,6 @@ package body BC.Containers.Rings is
     end;
   end Current_Item;
 
-  ------------------
-  -- Current_Item --
-  ------------------
-
   function Current_Item (It : Ring_Iterator) return Item_Ptr is
   begin
     if Is_Done (It) then
@@ -137,75 +154,6 @@ package body BC.Containers.Rings is
       end if;
       return Item_At (It.R.all, I);
     end;
---     return Item_At (It.R.all, It.Index);
   end Current_Item;
-
-  ----------------
-  -- Initialize --
-  ----------------
-
-  procedure Initialize (R : in out Ring) is
-  begin
-    R.Top := 0;
-    R.Mark := 0;
-  end Initialize;
-
-  ----------------
-  -- Initialize --
-  ----------------
-
-  procedure Initialize (It : in out Ring_Iterator) is
-  begin
-    Reset (It);
-  end Initialize;
-
-  -------------
-  -- Is_Done --
-  -------------
-
-  function Is_Done (It : Ring_Iterator) return Boolean is
-  begin
-    return It.Index = 0 or else It.Index > Cardinality (It.R.all);
-  end Is_Done;
-
-  ----------
-  -- Mark --
-  ----------
-
-  procedure Mark (R : in out Ring) is
-  begin
-    R.Mark := R.Top;
-  end Mark;
-
-  ----------
-  -- Next --
-  ----------
-
-  procedure Next (It : in out Ring_Iterator) is
-  begin
-    It.Index := It.Index + 1;
-  end Next;
-
-  -----------
-  -- Reset --
-  -----------
-
-  procedure Reset (It : in out Ring_Iterator) is
-  begin
-    if Cardinality (It.R.all) = 0 then
-      It.Index := 0;
-    else
-      It.Index := 1;
-    end if;
-  end Reset;
-
-  --------------------
-  -- Rotate_To_Mark --
-  --------------------
-
-  procedure Rotate_To_Mark (R : in out Ring) is
-  begin
-    R.Top := R.Mark;
-  end Rotate_To_Mark;
 
 end BC.Containers.Rings;
