35c5b2f358b9a84ec70bcc11c8ebc12b884d5fb7
Simon Wright <simon@pushface.org>
1999-04-12 21:07:54 +0000

    Removed C++ commentary. Included Lock, Unlock.

--- booch95/bc-containers-rings.ads
+++ booch95/bc-containers-rings.ads
@@ -27,45 +27,11 @@ package BC.Containers.Rings is
   -- beginning or ending, a client can mark one particular item to
   -- designate a point of reference in the structure.
 
---| //  The C++ Booch Components (Version 2.3)
---| //  (C) Copyright 1990-1994 Grady Booch. All Rights Reserved.
---| //
---| //  BCRing.h
---| //
---| //  This file contains the declaration of the ring abstract base class
---| //  and its iterators.
---|
---| #ifndef BCRING_H
---| #define BCRING_H 1
---|
---| #include "BCType.h"
---|
---| template<class Item>
---| class BC_TRingActiveIterator;
---|
---| template<class Item>
---| class BC_TRingPassiveIterator;
---|
---| template<class Item, class Structure>
---| class BC_TPersist;
---|
---| enum BC_Direction {BC_kForward, BC_kReverse};
+  -- XXX I am considerably confused by the use of "forward",
+  -- "backward", "clockwise" etc; some clarification wouldn't come
+  -- amiss!
 
   type Direction is (Forward, Backward); -- XXX why not clockwise, anticlockwise?
-
---|
---| // Ring abstract base class
---|
---| template<class Item>
---| class BC_TRing {
---| public:
---|
---|   BC_TRing();
---|   BC_TRing(const BC_TRing<Item>&);
---|   virtual ~BC_TRing();
---|
---|   virtual BC_TRing<Item>& operator=(const BC_TRing<Item>&);
---|   virtual BC_Boolean operator==(const BC_TRing<Item>&) const;
 
   function Are_Equal (Left, Right : Ring'Class) return Boolean;
   -- Return True if and only if both rings have the same extent and the
@@ -78,14 +44,8 @@ package BC.Containers.Rings is
   -- This operation MUST be called for dissimilar Rings in place of
   -- assignment.
 
---|   BC_Boolean operator!=(const BC_TRing<Item>&) const;
---|
---|   virtual void Clear() = 0;
-
   procedure Clear (R : in out Ring) is abstract;
   -- Empty the ring of all items. The mark is cleared.
-
---|   virtual void Insert(const Item&) = 0;
 
   procedure Insert (R : in out Ring; Elem : Item) is abstract;
   -- If the ring is empty, set the ring's mark to designate this
@@ -93,15 +53,11 @@ package BC.Containers.Rings is
   -- (if any) is now located clockwise adjacent to the new item; the
   -- old item is forward of the new one.
 
---|   virtual void Pop() = 0;
-
   procedure Pop (R : in out Ring) is abstract;
   -- Remove the item from the top of the ring. The clockwise adjacent
   -- item (if any) is now designated as the ring's top. If the removed
   -- item had been marked, the ring's new top (if not empty) is now
   -- designated as marked.
-
---|   virtual void Rotate(BC_Direction direction = BC_kForward) = 0;
 
   procedure Rotate (R : in out Ring; Dir : Direction := Forward) is abstract;
   -- Rotate the top of the ring in the given direction. Rotating the
@@ -111,35 +67,21 @@ package BC.Containers.Rings is
   -- exactly one item in the ring, rotating either direction always
   -- returns to the same item.
 
---|   virtual void Mark();
-
   procedure Mark (R : in out Ring);
   -- Designate the item at the top of the ring (if not empty) as
   -- marked.
 
---|   virtual void RotateToMark();
-
   procedure Rotate_To_Mark (R : in out Ring);
   -- Rotate the ring so that the ring's mark is at the top.
-
---|
---|   virtual BC_Index Extent() const = 0;
 
   function Extent (R : Ring) return Natural is abstract;
   -- Return the number of items in the ring.
 
---|   virtual BC_Boolean IsEmpty() const = 0;
-
   function Is_Empty (R : Ring) return Boolean is abstract;
   -- Return True if and only if there are no items in the ring.
 
---|   virtual const Item& Top() const = 0;
---|   virtual Item& Top() = 0;
-
   function Top (R : Ring) return Item is abstract;
   -- Return a copy of the item at the top of the ring.
-
---|   virtual BC_Boolean AtMark() const;
 
   function At_Mark (R : Ring) return Boolean;
   -- Return True if and only if the item at the top of the ring is
@@ -148,13 +90,7 @@ package BC.Containers.Rings is
   -- both do not designate any item.
   -- XXX hmm, odd logic there!
 
---|
---| protected:
-
 private
---|
---|   BC_ExtendedIndex fTop;
---|   BC_ExtendedIndex fMark;
 
   type Ring is abstract new Container with record
     Top : Natural;      -- 0 implies not set
@@ -163,68 +99,11 @@ private
 
   procedure Initialize (R : in out Ring); -- derivations will need to call this
 
-  procedure Adjust (R : in out Ring);
-
   procedure Add (R : in out Ring; Elem : Item);
 
---|
---|   virtual void Purge() = 0;
---|   virtual void Add(const Item&) = 0;
---|   virtual BC_Index Cardinality() const = 0;
---|   virtual const Item& ItemAt(BC_Index) const = 0;
---|
---|   virtual void Lock();
---|   virtual void Unlock();
---|
---| private:
---|
---|   friend class BC_TRingActiveIterator<Item>;
---|   friend class BC_TRingPassiveIterator<Item>;
---|
---|   friend class BC_TPersist<Item, BC_TRing<Item> >;
---|
---| };
---|
---| // Ring iterators
---|
---| template <class Item>
---| class BC_TRingActiveIterator {
---| public:
---|
---|   BC_TRingActiveIterator(const BC_TRing<Item>&);
---|   ~BC_TRingActiveIterator();
---|
---|   void Reset();
---|   BC_Boolean Next();
---|
---|   BC_Boolean IsDone() const;
---|   const Item* CurrentItem() const;
---|   Item* CurrentItem();
---|
---| protected:
---|
---|   const BC_TRing<Item>& fRing;
---|   BC_ExtendedIndex fIndex;
---|
---| };
---|
---| template <class Item>
---| class BC_TRingPassiveIterator {
---| public:
---|
---|   BC_TRingPassiveIterator(const BC_TRing<Item>&);
---|   ~BC_TRingPassiveIterator();
---|
---|   BC_Boolean Apply(BC_Boolean (*Process)(const Item&));
---|   BC_Boolean Apply(BC_Boolean (*Process)(Item&));
---|
---| protected:
---|
---|   const BC_TRing<Item>& fRing;
---|
---| };
---|
---| #endif
+  procedure Lock (R : in out Ring);
+
+  procedure Unlock (R : in out Ring);
 
   type Ring_Iterator (R : access Ring'Class)
   is new Actual_Iterator (R) with record
