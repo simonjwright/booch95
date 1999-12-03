efd71e06bcad5af4dd862a9f75a3c52b85566c93
simon <simon@localhost>
1999-12-03 20:57:12 +0000

    Add Delete_Item_At, to delete the Item an Iterator currently denotes. In all subprograms, the parameter name Obj is changed to L. Some reordering of private operations.

--- booch95/bc-containers-lists-single.ads
+++ booch95/bc-containers-lists-single.ads
@@ -34,77 +34,77 @@ package BC.Containers.Lists.Single is
   -- Return True if and only if both lists are null or structurally share
   -- the same list.
 
-  procedure Clear (Obj : in out Single_List);
+  procedure Clear (L : in out Single_List);
   -- If the list is not null, destroy this alias to the list, make the list
   -- null, and reclaim the storage associated with any unreachable items.
 
-  procedure Insert (Obj : in out Single_List; Elem : Item);
+  procedure Insert (L : in out Single_List; Elem : Item);
   -- Add the item to the head of the list.
 
-  procedure Insert (Obj : in out Single_List; From_List : in Single_List);
+  procedure Insert (L : in out Single_List; From_List : in Single_List);
   -- Add the given list to the head of the list.
 
-  procedure Insert (Obj : in out Single_List; Elem : Item; Before : Positive);
+  procedure Insert (L : in out Single_List; Elem : Item; Before : Positive);
   -- Add the item before the given index item in the list; if before is 1,
   -- the item is added to the head of the list.
 
-  procedure Insert (Obj : in out Single_List;
+  procedure Insert (L : in out Single_List;
                     From_List: in out Single_List;
                     Before : Positive);
   -- Add the list before the given index item in the list; if before is 1,
   -- the list is added to the head of the list.
 
-  procedure Append (Obj : in out Single_List; Elem : Item);
+  procedure Append (L : in out Single_List; Elem : Item);
   -- Add the item at the end of the list.
 
-  procedure Append (Obj : in out Single_List; From_List : in Single_List);
+  procedure Append (L : in out Single_List; From_List : in Single_List);
   -- Add the given list at the end of the list.
 
-  procedure Append (Obj : in out Single_List; Elem : Item; After : Positive);
+  procedure Append (L : in out Single_List; Elem : Item; After : Positive);
   -- Add the item after the given index item in the list.
 
-  procedure Append (Obj : in out Single_List;
+  procedure Append (L : in out Single_List;
                     From_List : in Single_List;
                     After : Positive);
   -- Add the list after the given index item in the list.
 
-  procedure Remove (Obj : in out Single_List; From : Positive);
+  procedure Remove (L : in out Single_List; From : Positive);
   -- Remove the item at the given index in the list.
 
-  procedure Purge (Obj : in out Single_List; From : Positive);
+  procedure Purge (L : in out Single_List; From : Positive);
   -- Remove all the items in the list starting at the given index,
   -- inclusive.
 
-  procedure Purge (Obj : in out Single_List; From : Positive; Count : Positive);
+  procedure Purge (L : in out Single_List; From : Positive; Count : Positive);
   -- Remove all the items in the list starting at the given index,
   -- inclusive, for a total of count items.
 
-  procedure Preserve (Obj : in out Single_List; From : Positive);
+  procedure Preserve (L : in out Single_List; From : Positive);
   -- Remove all the items in the list except those starting at the given
   -- index, inclusive.
 
-  procedure Preserve (Obj : in out Single_List;
+  procedure Preserve (L : in out Single_List;
                       From : Positive;
                       Count : Positive);
   -- Remove all the items in the list except those starting at the given
   -- index, inclusive, for a total of count items.
 
-  procedure Share (Obj : in out Single_List;
+  procedure Share (L : in out Single_List;
                    With_List: Single_List;
                    Starting_At : Positive);
   -- Clear the list, then, if the given list is not null, set the list to
   -- structurally share with the head of the given list, starting at the
   -- given index.
 
-  procedure Share_Head (Obj : in out Single_List; With_List : in Single_List);
+  procedure Share_Head (L : in out Single_List; With_List : in Single_List);
   -- Clear the list, then, if the given list is not null, set the list to
   -- structurally share with the head of the given list.
 
-  procedure Share_Foot (Obj : in out Single_List; With_List : in Single_List);
+  procedure Share_Foot (L : in out Single_List; With_List : in Single_List);
   -- Clear the list, then, if the given list is not null, set the list to
   -- structurally share with the end of the given list.
 
-  procedure Swap_Tail (Obj : in out Single_List;
+  procedure Swap_Tail (L : in out Single_List;
                        With_List : in out Single_List);
   -- The given list must represent the head of a list, which may be
   -- null. Set the tail of the list (which may be null) to denote the given
@@ -113,39 +113,39 @@ package BC.Containers.Lists.Single is
   -- list is set to be the head of the list. If it is not null, the
   -- predecessor of the new head of the given list is set to be null.
 
-  procedure Tail (Obj : in out Single_List);
+  procedure Tail (L : in out Single_List);
   -- The list must not be null. Set the list to now denote its tail (which
   -- may be null), and reclaim the storage associated with any unreachable
   -- items.
 
-  procedure Set_Head (Obj : in out Single_List; Elem : Item);
+  procedure Set_Head (L : in out Single_List; Elem : Item);
   -- Set the item at the head of the list.
 
-  procedure Set_Item (Obj : in out Single_List;
+  procedure Set_Item (L : in out Single_List;
                       Elem : Item;
                       At_Loc : Positive);
   -- Set the item at the given index.
 
-  function Length (Obj : Single_List) return Natural;
+  function Length (L : Single_List) return Natural;
   -- Return the number of items in the list.
 
-  function Is_Null (Obj : Single_List) return Boolean;
+  function Is_Null (L : Single_List) return Boolean;
   -- Return True if and only there are no items in the list.
 
-  function Is_Shared (Obj : Single_List) return Boolean;
+  function Is_Shared (L : Single_List) return Boolean;
   -- Return True if and only if the list has an alias.
 
-  function Head (Obj : Single_List) return Item;
+  function Head (L : Single_List) return Item;
   -- Return a copy of the item at the head of the list.
 
   -- XXX need accessor generic
 
-  function Foot (Obj : Single_List) return Item;
+  function Foot (L : Single_List) return Item;
   -- Return a copy of the item at the end of the list.
 
   -- XXX need accessor generic
 
-  function Item_At (Obj : Single_List; Index : Positive) return Item;
+  function Item_At (L : Single_List; Index : Positive) return Item;
   -- Return a copy of the item at the given index.
 
   function New_Iterator (For_The_List : Single_List) return Iterator;
@@ -153,8 +153,8 @@ package BC.Containers.Lists.Single is
 
 private
 
-  function Item_At (Obj : Single_List; Index : Positive) return Item_Ptr;
-  function Cardinality (Obj : Single_List) return Natural;
+  function Cardinality (L : Single_List) return Natural;
+  function Item_At (L : Single_List; Index : Positive) return Item_Ptr;
 
   package Single_Nodes
   is new BC.Support.Nodes (Item, Storage_Manager, Storage);
@@ -163,9 +163,9 @@ private
     Rep : Single_Nodes.Single_Node_Ref;
   end record;
 
-  procedure Initialize (Obj : in out Single_List);
-  procedure Adjust (Obj : in out Single_List);
-  procedure Finalize (Obj : in out Single_List);
+  procedure Initialize (L : in out Single_List);
+  procedure Adjust (L : in out Single_List);
+  procedure Finalize (L : in out Single_List);
 
   type Single_List_Iterator (L : access Single_List'Class)
   is new Actual_Iterator (L) with record
@@ -185,5 +185,7 @@ private
   function Current_Item (It : Single_List_Iterator) return Item;
 
   function Current_Item (It : Single_List_Iterator) return Item_Ptr;
+
+  procedure Delete_Item_At (It : Single_List_Iterator);
 
 end BC.Containers.Lists.Single;
