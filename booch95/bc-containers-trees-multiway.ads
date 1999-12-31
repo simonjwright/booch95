efdb70becb3bcea08e074575c7e26cf0d1be4b3e
simon <simon@localhost>
1999-12-31 15:15:46 +0000

    Added Elaborate_Body. Changed Obj to T.

--- booch95/bc-containers-trees-multiway.ads
+++ booch95/bc-containers-trees-multiway.ads
@@ -27,6 +27,8 @@ generic
   Storage : in out Storage_Manager;
 package BC.Containers.Trees.Multiway is
 
+  pragma Elaborate_Body;
+
   type Multiway_Tree is private;
 
   function Create (From : Multiway_Tree) return Multiway_Tree;
@@ -37,37 +39,37 @@ package BC.Containers.Trees.Multiway is
   -- Return True if and only if both trees are null or structurally share the
   -- same tree.
 
-  procedure Clear (Obj : in out Multiway_Tree);
+  procedure Clear (T : in out Multiway_Tree);
   -- If the tree is not null, destroy this alias to the tree, make the tree
   -- null, and reclaim the storage associated with any unreachable items.
 
-  procedure Insert (Obj : in out Multiway_Tree; Elem : in Item);
+  procedure Insert (T : in out Multiway_Tree; Elem : in Item);
   -- Add the item to the root of the tree and make the original root the
   -- immediate child of this new tree.
 
-  procedure Append (Obj : in out Multiway_Tree;
+  procedure Append (T : in out Multiway_Tree;
                     Elem : in Item);
   -- Add the item as the immediate child of the tree.
 
-  procedure Append (Obj : in out Multiway_Tree;
+  procedure Append (T : in out Multiway_Tree;
                     Elem : in Item;
                     After : Positive);
   -- Add the item as a child of the tree, after the given indexed child.
 
-  procedure Append (Obj : in out Multiway_Tree;
+  procedure Append (T : in out Multiway_Tree;
                     From_Tree : in out Multiway_Tree);
   -- Add the tree as the immediate child of the tree.
 
-  procedure Remove (Obj : in out Multiway_Tree; Index : Positive);
+  procedure Remove (T : in out Multiway_Tree; Index : Positive);
   -- Remove the given child and destroy it if it is no longer reachable.
 
-  procedure Share (Obj : in out Multiway_Tree;
+  procedure Share (T : in out Multiway_Tree;
                    Share_With : in Multiway_Tree;
                    Child : Positive);
   -- Clear the tree, then, if the given tree is not null, set the tree to
   -- structurally share with the given child of the tree.
 
-  procedure Swap_Child (Obj : in out Multiway_Tree;
+  procedure Swap_Child (T : in out Multiway_Tree;
                         Swap_WIth : in out Multiway_Tree;
                         Child : in Positive);
   -- The given tree must represent the root of a tree, which may be
@@ -77,33 +79,33 @@ package BC.Containers.Trees.Multiway is
   -- the tree is set to be the root of the tree.  If it is not null, the
   -- parent of the new root of the given tree is set to be null.
 
-  procedure Child (Obj : in out Multiway_Tree; Child : in Positive);
+  procedure Child (T : in out Multiway_Tree; Child : in Positive);
   -- The tree must not be null. Set the tree to now denote the given child
   -- (which may be null) and reclaim the storage associated with any
   -- unreachable items.
 
-  procedure Parent (Obj : in out Multiway_Tree);
+  procedure Parent (T : in out Multiway_Tree);
   -- Set the tree to now denote its parent (if any).
 
-  procedure Set_Item (Obj : in out Multiway_Tree; Elem : in Item);
+  procedure Set_Item (T : in out Multiway_Tree; Elem : in Item);
   -- Set the item at the root of the tree.
 
-  function Arity (Obj : Multiway_Tree) return Natural;
+  function Arity (T : Multiway_Tree) return Natural;
   -- Return the number of children relative to the root of the tree.
 
-  function Has_Children (Obj : in Multiway_Tree) return Boolean;
+  function Has_Children (T : in Multiway_Tree) return Boolean;
   -- Return True if and only if the tree has any non-null children.
 
-  function Is_Null (Obj : in Multiway_Tree) return Boolean;
+  function Is_Null (T : in Multiway_Tree) return Boolean;
   -- Return True if and only if the tree has no items.
 
-  function Is_Shared (Obj : in Multiway_Tree) return Boolean;
+  function Is_Shared (T : in Multiway_Tree) return Boolean;
   -- Return True if and only if the tree has an alias.
 
-  function Is_Root (Obj : in Multiway_Tree) return Boolean;
+  function Is_Root (T : in Multiway_Tree) return Boolean;
   -- Return True if and only if the tree is at the root of a tree.
 
-  function Item_At (Obj : in Multiway_Tree) return Item;
+  function Item_At (T : in Multiway_Tree) return Item;
   -- Return the item at the root of the tree.
 
 private
@@ -114,8 +116,8 @@ private
     Rep : Nodes.Multiway_Node_Ref;
   end record;
 
-  procedure Initialize (Obj : in out Multiway_Tree);
-  procedure Adjust (Obj : in out Multiway_Tree);
-  procedure Finalize (Obj : in out Multiway_Tree);
+  procedure Initialize (T : in out Multiway_Tree);
+  procedure Adjust (T : in out Multiway_Tree);
+  procedure Finalize (T : in out Multiway_Tree);
 
 end BC.Containers.Trees.Multiway;
