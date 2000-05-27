183d3f45ea72a5c17402565bfb6b3ebbc222a99f
simon <simon@localhost>
2000-05-27 16:55:39 +0000

    Updated for Fast Iterators. The Map-specific iteration support now requires Map_Iterators, although New_Iterators still return classwide Iterators. This is not neat.

--- booch95/bc-containers-maps.ads
+++ booch95/bc-containers-maps.ads
@@ -88,19 +88,21 @@ package BC.Containers.Maps is
 
   -- Additional Iterator support
 
-  function Current_Value (It : Iterator) return Value;
+  type Map_Iterator is new Iterator with private;
+
+  function Current_Value (It : Map_Iterator) return Value;
   -- Return a copy of the current Value.
 
   generic
     with procedure Apply (I : Item; V : Value; OK : out Boolean);
-  procedure Visit (Using : in out Iterator);
+  procedure Visit (Using : in out Map_Iterator'Class);
   -- Call Apply with a copy of each Item/Value pair in the Container
   -- to which the iterator Using is bound. The iteration will
   -- terminate early if Apply sets OK to False.
 
   generic
     with procedure Apply (I : Item; V : in out Value; OK : out Boolean);
-  procedure Modify (Using : in out Iterator);
+  procedure Modify (Using : in out Map_Iterator'Class);
   -- Call Apply for each Item/Value pair in the Container to which the
   -- iterator Using is bound. The Item is a copy, the Value is the
   -- actual content. The iteration will terminate early if Apply sets
@@ -123,13 +125,19 @@ private
 
   function Value_At (M : Map; Bucket, Index : Positive) return Value_Ptr;
 
-  type Map_Iterator (M : access Map'Class)
-  is new Actual_Iterator (M) with record
+  -- The new subprograms for Map iteration (which allow access to the
+  -- Value as well as the Item) require the inherited For_The_Container
+  -- to in fact be in Map'Class. This must be the case since the only way
+  -- of getting a Map_Iterator is by using one of the concrete forms'
+  -- New_Iterator using eg
+  --
+  --   Iter : Map_Iterator'Class := Map_Iterator'Class (New_Iterator (M));
+  --
+  -- which GNAT fails at compilation time if M isn't actually a Map.
+  type Map_Iterator is new Iterator with record
     Bucket_Index : Natural := 0;
     Index : Natural := 0;
   end record;
-
-  procedure Initialize (It : in out Map_Iterator);
 
   -- Overriding primitive supbrograms of the concrete actual Iterator.
 
