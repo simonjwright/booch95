446eea0751b1a829f9e7f467e037d8edb5b2f38e
Simon Wright <simon@pushface.org>
2000-11-18 18:05:24 +0000

    The old Semaphore becomes Recursive_Semaphore; the new Semaphore is like a plain mutex. Added some commentary. Moved the Controlled operations to the visible part.

--- booch95/bc-support-synchronization.ads
+++ booch95/bc-support-synchronization.ads
@@ -24,6 +24,7 @@ package BC.Support.Synchronization is
 
   pragma Elaborate_Body;
 
+  -- Semaphores provide for mutual exclusion.
   type Semaphore_Base is abstract new Ada.Finalization.Controlled with private;
   procedure Seize (The_Semaphore : in out Semaphore_Base) is abstract;
   procedure Release (The_Semaphore : in out Semaphore_Base) is abstract;
@@ -34,16 +35,30 @@ package BC.Support.Synchronization is
   procedure Delete (The_Semaphore : in out Semaphore_P);
 
 
-  -- A Semaphore is like a POSIX recursive mutex; once Seized by a
-  -- task, that task can Seize again; other tasks are blocked until
-  -- the owning task has Released the semaphore as many times as it
-  -- Seized it.
+  -- A Semaphore is like a standard POSIX mutex.
   type Semaphore is new Semaphore_Base with private;
+  procedure Initialize (The_Semaphore : in out Semaphore);
+  procedure Adjust (The_Semaphore : in out Semaphore);
+  procedure Finalize (The_Semaphore : in out Semaphore);
   procedure Seize (The_Semaphore : in out Semaphore);
   procedure Release (The_Semaphore : in out Semaphore);
   function None_Pending (On_The_Semaphore : Semaphore) return Boolean;
 
 
+  -- A Recursive_Semaphore is like a POSIX recursive mutex; once
+  -- Seized by a task, that task can Seize again; other tasks are
+  -- blocked until the owning task has Released the semaphore as many
+  -- times as it Seized it.
+  type Recursive_Semaphore is new Semaphore_Base with private;
+  procedure Initialize (The_Semaphore : in out Recursive_Semaphore);
+  procedure Adjust (The_Semaphore : in out Recursive_Semaphore);
+  procedure Finalize (The_Semaphore : in out Recursive_Semaphore);
+  procedure Seize (The_Semaphore : in out Recursive_Semaphore);
+  procedure Release (The_Semaphore : in out Recursive_Semaphore);
+  function None_Pending (On_The_Semaphore : Recursive_Semaphore) return Boolean;
+
+
+  -- Monitors support Locks.
   type Monitor_Base is abstract tagged limited private;
   procedure Seize_For_Reading (The_Monitor : in out Monitor_Base)
     is abstract;
@@ -58,6 +73,8 @@ package BC.Support.Synchronization is
   procedure Delete (The_Monitor : in out Monitor_P);
 
 
+  -- Single_Monitors allow one task at a time to have access, be it
+  -- for reading or writing.
   type Single_Monitor is new Monitor_Base with private;
   procedure Seize_For_Reading (The_Monitor : in out Single_Monitor);
   procedure Seize_For_Writing (The_Monitor : in out Single_Monitor);
@@ -65,6 +82,8 @@ package BC.Support.Synchronization is
   procedure Release_From_Writing (The_Monitor : in out Single_Monitor);
 
 
+  -- Multiple_Monitors allow multiple readers; however, when a writer
+  -- owns the monitor it has exclusive access.
   type Multiple_Monitor is new Monitor_Base with private;
   procedure Seize_For_Reading (The_Monitor : in out Multiple_Monitor);
   procedure Seize_For_Writing (The_Monitor : in out Multiple_Monitor);
@@ -72,15 +91,25 @@ package BC.Support.Synchronization is
   procedure Release_From_Writing (The_Monitor : in out Multiple_Monitor);
 
 
+  -- A Lock is designed to provide "locking by declaration".
+  --   declare
+  --     L : Lock (Some_Monitor'Access);
+  --   begin
+  --     -- the monitor is locked
+  --   end;
+  --   -- the monitor is unlocked as L is finalized, even if an exception
+  --   -- occurs
+
   type Lock_Base
     is abstract new Ada.Finalization.Limited_Controlled with private;
 
-  type Lock_P is access all Lock_Base'Class;
-  procedure Delete (The_Lock : in out Lock_P);
-
+  -- A simple Lock provides mutual exclusion
   type Lock (Using : access Semaphore_Base'Class)
   is new Lock_Base with private;
 
+  -- Read_ and Write_ Locks support multiple reader/single writer
+  -- access provided the given Monitor supports it; otherwise it
+  -- merely provides mutual exclusion.
   type Read_Lock (Using : access Monitor_Base'Class)
   is new Lock_Base with private;
 
@@ -97,9 +126,7 @@ private
     procedure Release;
     function None_Pending return Boolean;
   private
-    entry Waiting;
-    Owner : Ada.Task_Identification.Task_Id;
-    Count : Natural := 0;
+    Seized : Boolean := False;
   end Semaphore_Type;
 
   type Semaphore_Type_P is access all Semaphore_Type;
@@ -107,14 +134,27 @@ private
   type Semaphore is new Semaphore_Base with record
     S : Semaphore_Type_P;
   end record;
-  procedure Initialize (The_Semaphore : in out Semaphore);
-  procedure Adjust (The_Semaphore : in out Semaphore);
-  procedure Finalize (The_Semaphore : in out Semaphore);
+
+  protected type Recursive_Semaphore_Type is
+    entry Seize;
+    procedure Release;
+    function None_Pending return Boolean;
+  private
+    entry Waiting;
+    Owner : Ada.Task_Identification.Task_Id;
+    Count : Natural := 0;
+  end Recursive_Semaphore_Type;
+
+  type Recursive_Semaphore_Type_P is access all Recursive_Semaphore_Type;
+
+  type Recursive_Semaphore is new Semaphore_Base with record
+    S : Recursive_Semaphore_Type_P;
+  end record;
 
   type Monitor_Base is abstract tagged limited null record;
 
   type Single_Monitor is new Monitor_Base with record
-    The_Semaphore : Semaphore;
+    The_Semaphore : Recursive_Semaphore;
   end record;
 
   -- Monitor_Type is due to Matthew Heaney <matthew_heaney@acm.org>.
