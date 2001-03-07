3fe65a7f82e2f83f12e0c513093c2e8cb5b8bb51
simon <simon@localhost>
2001-03-07 20:36:36 +0000

    Check the ability to re-insert an element that compares equal but whose non-key fields have changed.

--- booch95/avl_test_support.ads
+++ booch95/avl_test_support.ads
@@ -1,4 +1,4 @@
--- Copyright (C) 1999 Simon Wright.
+-- Copyright (C) 1999, 2001 Simon Wright.
 -- All Rights Reserved.
 --
 -- This program is distributed in the hope that it will be
@@ -22,7 +22,16 @@ with Global_Heap;
 
 package AVL_Test_Support is
 
-  type Item is range 0 .. 1023;
+  type Key is range 0 .. 1023;
+
+  type Item is record
+    K : Key;
+    Count : Integer := 0;
+  end record;
+
+  function "=" (L, R : Item) return Boolean;
+  function "<" (L, R : Item) return Boolean;
+  function Image (Val : Item) return String;
 
   package Containers is new BC.Containers (Item => Item);
 
@@ -31,7 +40,7 @@ package AVL_Test_Support is
   package TA is new Trees.AVL (Storage_Manager => Global_Heap.Pool,
                                Storage => Global_Heap.Storage);
 
-  procedure Print is new TA.Print (Image => Item'Image);
+  procedure Print is new TA.Print (Image => Image);
 
   procedure Validate is new TA.Validate;
 
