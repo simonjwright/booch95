c76e03006bae02fd4034aef0554ce9cf35e66580
Simon Wright <simon@pushface.org>
2001-03-07 20:36:36 +0000

    Check the ability to re-insert an element that compares equal but whose non-key fields have changed.

--- booch95/avl_test.adb
+++ booch95/avl_test.adb
@@ -27,29 +27,49 @@ procedure AVL_Test is
   use AVL_Test_Support;
   use TA;
 
-  package Rand is new Ada.Numerics.Discrete_Random (Item);
+  package Rand is new Ada.Numerics.Discrete_Random (Key);
   use Rand;
   G : Generator;
 
   T : AVL_Tree;
   Loops : Natural := 0;
 
+  Inserted : Boolean;
+
 begin
 
-  Put_Line ("Starting AVL tests");
+  Put_Line ("Starting AVL insertion/replacement tests");
+
+  Insert (T => T,
+          Element => (K => 497, Count => 42),
+          Not_Found => Inserted);
+  Insert (T => T,
+          Element => (K => 42, Count => 497),
+          Not_Found => Inserted);
+  Print (T);
+  Insert (T => T,
+          Element => (K => 497, Count => 321),
+          Not_Found => Inserted);
+  Insert (T => T,
+          Element => (K => 42, Count => 123),
+          Not_Found => Inserted);
+  Validate (T);
+  Print (T);
+
+  Put_Line ("Starting AVL insertion/deletion tests");
 
   Reset (G);
 
-  -- insert and remove random items until the tree is a half full
-  while Extent (T) < Natural (Item'Last / 2) loop
+  -- insert and remove random items until the tree is half full
+  while Extent (T) < Natural (Key'Last / 2) loop
     declare
       Dummy : Boolean;
     begin
       Insert (T => T,
-              Element => Random (G),
+              Element => (K => Random (G), Count => 1234),
               Not_Found => Dummy);
       Delete (T => T,
-              Element => Random (G),
+              Element => (K => Random (G), Count => 2345),
               Found => Dummy);
       Validate (T);
       Loops := Loops + 1;
