478c443b785cc1a2b32ca9daa88036f2c0b39316
Simon Wright <simon@pushface.org>
2001-03-22 20:20:09 +0000

    First version.

---
+++ booch95/bc-copy.adb
@@ -0,0 +1,33 @@
+-- Copyright (C) 2001 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+procedure BC.Copy (Input : From; Output : in out To) is
+  procedure Ins (I : Item; OK : out Boolean);
+  pragma Inline (Ins);
+  procedure Cp is new Source.Visit (Ins);
+  procedure Ins (I : Item; OK : out Boolean) is
+  begin
+    Add (Output, I);
+    OK := True;
+  end Ins;
+  It : Source.Iterator'Class := New_Iterator (Input);
+begin
+  Clear (Output);
+  Cp (It);
+end BC.Copy;
---
+++ booch95/bc-filter.adb
@@ -0,0 +1,35 @@
+-- Copyright (C) 2001 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+procedure BC.Filter (Input : From; Output : in out To) is
+  procedure Ins (I : Item; OK : out Boolean);
+  pragma Inline (Ins);
+  procedure Filt is new Source.Visit (Ins);
+  procedure Ins (I : Item; OK : out Boolean) is
+  begin
+    if Pass (I) then
+      Add (Output, I);
+    end if;
+    OK := True;
+  end Ins;
+  It : Source.Iterator'Class := New_Iterator (Input);
+begin
+  Clear (Output);
+  Filt (It);
+end BC.Filter;
