bbf8b3ba24343729b128fa9d1cc856de2f994fec
simon <simon@localhost>
2001-03-22 20:20:09 +0000

    First version.

---
+++ booch95/bc-filter.ads
@@ -0,0 +1,30 @@
+-- Copyright (C) 2001 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+with Bc.Containers;
+generic
+  type Item is private;
+  with package Source is new BC.Containers (Item);
+  type From is new Source.Container with private;
+  with package Target is new BC.Containers (Item);
+  type To is new Target.Container with private;
+  with function Pass (I : Item) return Boolean is <>;
+  with procedure Clear (The_Container : in out To) is <>;
+  with procedure Add (To_The_Container : in out To; I : Item) is <>;
+procedure BC.Filter (Input : From; Output : in out To);
