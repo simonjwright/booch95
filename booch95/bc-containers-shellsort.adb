510cc86f053774b1602863ea48cee53293c622c1
simon <simon@localhost>
2001-04-03 18:39:51 +0000

    First version.

---
+++ booch95/bc-containers-shellsort.adb
@@ -0,0 +1,55 @@
+-- Copyright (C) 2001 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+-- Algorithm from "Algorithms", Robert Sedgewick, Addison-Wesley 1983
+
+procedure BC.Containers.Shellsort (C : in out Container) is
+  procedure Sort (C : in out Containers.Container'Class);
+  pragma Inline (Sort);
+  Len : constant Natural := Length (C);
+  procedure Sort (C : in out Containers.Container'Class) is
+    H : Positive := 1;
+    J : Positive;
+    V : Item;
+  begin
+    loop
+      H := 3 * H + 1;
+      exit when H > Len;
+    end loop;
+    loop
+      H := H / 3;
+      for I in H + 1 .. Len loop
+        V := Item_At (C, I).all;
+        J := I;
+        while V < Item_At (C, J - H).all loop
+          Item_At (C, J).all := Item_At (C, J - H).all;
+          J := J - H;
+          exit when J <= H;
+        end loop;
+        Item_At (C, J).all := V;
+      end loop;
+      exit when H = 1;
+    end loop;
+  end Sort;
+begin
+  Sort (C);
+exception
+  when Should_Have_Been_Overridden => raise Container_Error;
+end BC.Containers.Shellsort;
+
---
+++ booch95/bc-containers-shellsort.ads
@@ -0,0 +1,36 @@
+-- Copyright (C) 2001 Simon Wright.
+-- All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+-- $Id$
+
+-- Implements a Shell sort of the given container in place, using the
+-- supplied comparator.
+--
+-- The instantiating Container must fully support the operation
+--
+--   function Item_At (C : Container; Index : Positive) return Item_Ptr;
+--
+-- which is normally private and whose default implementation raises
+-- Should_Have_Been_Overridden. If it's not fully supported, as is the
+-- case for hash-table-based containers (Bags, Maps, Sets),
+-- Container_Error will be raised.
+
+generic
+  with function "<" (L, R : Item) return Boolean is <>;
+  type Container is new Containers.Container with private;
+  with function Length (C : Container) return Natural is <>;
+procedure BC.Containers.Shellsort (C : in out Container);
