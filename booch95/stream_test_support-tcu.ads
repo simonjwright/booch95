56d6a2716ed5ffe6833a9213fe680b9a7f13d4a4
Simon Wright <simon@pushface.org>
2002-01-12 11:54:23 +0000

    First version.

---
+++ booch95/stream_test_support-tcu.ads
@@ -0,0 +1,24 @@
+--  Copyright (C) 2002 Simon Wright.
+--  All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+--  $Id$
+
+with BC.Containers.Collections.Unbounded;
+with BC.Support.Standard_Storage;
+
+package Stream_Test_Support.TCU is new Abstract_Base_Collections.Unbounded
+  (Storage => BC.Support.Standard_Storage.Pool);
