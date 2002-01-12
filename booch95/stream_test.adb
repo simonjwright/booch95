2c78224cba21e9ddca5d8876478e86aa785f1aba
simon <simon@localhost>
2002-01-12 11:40:46 +0000

    Stream tests on actual Collections.

--- booch95/stream_test.adb
+++ booch95/stream_test.adb
@@ -24,6 +24,9 @@ with Assertions;
 with Collection_Test_Support;
 with Set_Test_Support;
 with Stream_Test_Support;
+with Stream_Test_Support.TCB;
+with Stream_Test_Support.TCD;
+with Stream_Test_Support.TCU;
 
 procedure Stream_Test is
 
@@ -53,6 +56,20 @@ procedure Stream_Test is
          Add (S, Using (I));
       end loop;
    end Setup;
+
+   procedure Image (C : Abstract_Base_Containers.Container'Class);
+   procedure Image (C : Abstract_Base_Containers.Container'Class) is
+      It : Abstract_Base_Containers.Iterator'Class :=
+        Abstract_Base_Containers.New_Iterator (C);
+      use Abstract_Base_Containers;
+   begin
+      Put_Line ("start");
+      while not Is_Done (It) loop
+         Ada.Text_IO.Put_Line ("  " & Image (Current_Item (It)));
+         Next (It);
+      end loop;
+      Put_Line ("end");
+   end Image;
 
    F : Ada.Streams.Stream_IO.File_Type;
 
@@ -164,6 +181,26 @@ begin
    end;
 
    declare
+      C1, C2 : ICD.Collection;
+      use ICD;
+   begin
+
+      Put_Line ("...Dynamic complex Collections");
+
+      Reset (F, Mode => Out_File);
+      Append (C1, (Of_Kind => I, I => 1234));
+      Append (C1, (Of_Kind => C, C => 'Z'));
+      Append (C1, (Of_Kind => Stream_Test_Support.F, F => 0.54321));
+      Clear (C2);
+      Assertion (C1 /= C2, "ICD1: Collections are equal");
+      Collection'Output (Stream (F), C1);
+      Reset (F, Mode => In_File);
+      C2 := Collection'Input (Stream (F));
+      Assertion (C1 = C2, "ICD2: Collections are unequal");
+
+   end;
+
+   declare
       C1, C2 : ICU.Collection;
       use ICU;
    begin
@@ -189,6 +226,8 @@ begin
 
    begin
 
+      Put_Line ("...Classwide pointers");
+
       Reset (F, Mode => Out_File);
 
       B1 := new Brother'(I => 16#aabb#);
@@ -204,6 +243,68 @@ begin
 
       Assertion (B2.all = B1.all, "P1: values are unequal");
       Assertion (S2.all = S1.all, "P2: values are unequal");
+
+   end;
+
+   declare
+      C1, C2 : TCB.Collection;
+      use TCB;
+   begin
+
+      Put_Line ("...Bounded tagged Collections");
+
+      Reset (F, Mode => Out_File);
+      Append (C1, new Brother'(I => 16#aabb#));
+      Append (C1, new Sister'(B => True));
+      Append (C2, new Brother'(I => 16#5555#));
+      Append (C2, new Sister'(B => False));
+      Assertion (C1 /= C2, "TCB1: Collections are equal");
+      Collection'Output (Stream (F), C1);
+      Reset (F, Mode => In_File);
+      C2 := Collection'Input (Stream (F));
+      Assertion (C1 = C2, "TCB2: Collections are unequal");
+
+   end;
+
+   declare
+      C1, C2 : TCD.Collection;
+      use TCD;
+   begin
+
+      Put_Line ("...Dynamic tagged Collections");
+
+      Reset (F, Mode => Out_File);
+      Append (C1, new Brother'(I => 16#aabb#));
+      Append (C1, new Sister'(B => True));
+      Append (C2, new Brother'(I => 16#5555#));
+      Append (C2, new Sister'(B => False));
+      Assertion (C1 /= C2, "TCD1: Collections are equal");
+      Collection'Output (Stream (F), C1);
+      Reset (F, Mode => In_File);
+      C2 := Collection'Input (Stream (F));
+      Image (C1);
+      Image (C2);
+      Assertion (C1 = C2, "TCD2: Collections are unequal");
+
+   end;
+
+   declare
+      C1, C2 : TCU.Collection;
+      use TCU;
+   begin
+
+      Put_Line ("...Unbounded tagged Collections");
+
+      Reset (F, Mode => Out_File);
+      Append (C1, new Brother'(I => 16#aabb#));
+      Append (C1, new Sister'(B => True));
+      Append (C2, new Brother'(I => 16#5555#));
+      Append (C2, new Sister'(B => False));
+      Assertion (C1 /= C2, "TCU1: Collections are equal");
+      Collection'Output (Stream (F), C1);
+      Reset (F, Mode => In_File);
+      C2 := Collection'Input (Stream (F));
+      Assertion (C1 = C2, "TCU2: Collections are unequal");
 
    end;
 
