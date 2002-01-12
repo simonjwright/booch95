1ae87a37907c8ca8db2a358cd22dc7cf3250f66d
simon <simon@localhost>
2002-01-12 11:54:23 +0000

    First version.

---
+++ booch95/stream_test_support-tcb.ads
@@ -0,0 +1,23 @@
+--  Copyright (C) 2002 Simon Wright.
+--  All Rights Reserved.
+--
+--      This program is free software; you can redistribute it
+--      and/or modify it under the terms of the Ada Community
+--      License which comes with this Library.
+--
+--      This program is distributed in the hope that it will be
+--      useful, but WITHOUT ANY WARRANTY; without even the implied
+--      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--      PURPOSE. See the Ada Community License for more details.
+--      You should have received a copy of the Ada Community
+--      License with this library, in the file named "Ada Community
+--      License" or "ACL". If not, contact the author of this library
+--      for a copy.
+--
+
+--  $Id$
+
+with BC.Containers.Collections.Bounded;
+
+package Stream_Test_Support.TCB is new Abstract_Base_Collections.Bounded
+  (Maximum_Size => 100);
