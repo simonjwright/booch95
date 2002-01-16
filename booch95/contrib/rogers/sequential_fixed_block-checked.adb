94cae40f6432c1f139208e8f467a5b2e475a0460
simon <simon@localhost>
2002-01-16 05:20:58 +0000

    Reformatted for -gnaty.

--- booch95/contrib/rogers/sequential_fixed_block-checked.adb
+++ booch95/contrib/rogers/sequential_fixed_block-checked.adb
@@ -1,29 +1,35 @@
 package body Sequential_Fixed_Block.Checked is
 
-  use type SSE.Storage_Offset;
+
+   use type SSE.Storage_Offset;
 
 
-  procedure Deallocate(	Pool            : in out Storage_Pool;
-                        Storage_Address : in     System.Address;
-                        Allocation      : in     SSE.Storage_Count;
-                        Alignment       : in     SSE.Storage_Count ) is
-    Ptr : SSE.Storage_Offset;
-    use type System.Address;
-  begin
-    Ptr := Pool.Next_Free;
-    while Ptr /= Null_Pointer loop
-      if Pool.Content(Ptr)'Address = Storage_Address then
-        -- previously deallocated
-        return;
-      end if;
-      Ptr := Value_At( Pool.Content(Ptr)'Address );
-    end loop;
-    -- not Previously_Deallocated
-    -- place index of next free element into this space (treated as a storage_offset value)
-    Place( Pool.Next_Free, Storage_Address );
-    -- update next_free index to indicate this newly-freed space
-    Pool.Next_Free := Storage_Address - Pool.Content'Address + Pool.Content_Alignment_Offset + 1;
-  end Deallocate;
+   procedure Deallocate (Pool            : in out Storage_Pool;
+                         Storage_Address : in     System.Address;
+                         Allocation      : in     SSE.Storage_Count;
+                         Alignment       : in     SSE.Storage_Count) is
+      Ptr : SSE.Storage_Offset;
+      use type System.Address;
+   begin
+      Ptr := Pool.Next_Free;
+      while Ptr /= Null_Pointer loop
+         if Pool.Content (Ptr)'Address = Storage_Address then
+            --  previously deallocated
+            return;
+         end if;
+         Ptr := Value_At (Pool.Content (Ptr)'Address);
+      end loop;
+      --  not Previously_Deallocated
+      --  place index of next free element into this space (treated as
+      --  a storage_offset value)
+      Place (Pool.Next_Free, Storage_Address);
+      --  update next_free index to indicate this newly-freed space
+      Pool.Next_Free :=
+        Storage_Address
+        - Pool.Content'Address
+        + Pool.Content_Alignment_Offset
+        + 1;
+   end Deallocate;
 
 
 end Sequential_Fixed_Block.Checked;
--- booch95/contrib/rogers/sequential_fixed_block-checked.ads
+++ booch95/contrib/rogers/sequential_fixed_block-checked.ads
@@ -2,21 +2,22 @@ with System.Storage_Elements;
 
 package Sequential_Fixed_Block.Checked is
 
-  -- Instances of Unchecked_Deallocation, which call Deallocate, will check that
-  -- prior calls with the same access value do not attempt to deallocate the
-  -- same access value, thereby preventing heap corruption.
+   --  Instances of Unchecked_Deallocation, which call Deallocate, will
+   --  check that prior calls with the same access value do not attempt
+   --  to deallocate the same access value, thereby preventing heap
+   --  corruption.
 
-  pragma Elaborate_Body;
+   pragma Elaborate_Body;
 
-  package SSE renames System.Storage_Elements;
+   package SSE renames System.Storage_Elements;
 
-  type Storage_Pool is
-    new Sequential_Fixed_Block.Storage_Pool with null record;
+   type Storage_Pool is
+     new Sequential_Fixed_Block.Storage_Pool with null record;
 
-  procedure Deallocate(	Pool            : in out Storage_Pool;
-                        Storage_Address : in     System.Address;
-                        Allocation      : in     SSE.Storage_Count;
-                        Alignment       : in     SSE.Storage_Count );
-	
+   procedure Deallocate (Pool            : in out Storage_Pool;
+                         Storage_Address : in     System.Address;
+                         Allocation      : in     SSE.Storage_Count;
+                         Alignment       : in     SSE.Storage_Count);
+
 end Sequential_Fixed_Block.Checked;
 
