8283539b7bded2ca77c81b005ed681e5d58c7e53
Simon Wright <simon@pushface.org>
2002-07-27 16:45:07 +0000

    The maximum size specified at instantiation is now a default maximum size; the actual maximum size is supplied as a constraint.

--- booch95/bc-support-bounded.adb
+++ booch95/bc-support-bounded.adb
@@ -29,13 +29,6 @@ package body BC.Support.Bounded is
    procedure Assert
    is new BSE.Assert ("BC.Support.Bounded");
 
-   --  We can't take 'Access of components of constant (in parameter)
-   --  objects; but we need to be able to do this so that we can
-   --  return pointers to individual elements. This technique is due
-   --  to Matthew Heaney.
-   package Allow_Access
-   is new System.Address_To_Access_Conversions (Elem_Array);
-
    --  We can't take 'Access of non-aliased components. But if we
    --  alias discriminated objects they become constrained - even if
    --  the discriminant has a default.
@@ -58,17 +51,17 @@ package body BC.Support.Bounded is
 
    procedure Clear (Obj : in out Bnd_Node) is
    begin
-      Obj.Start := 0;
+      Obj.Start := 1;
       Obj.Size := 0;
    end Clear;
 
    procedure Insert (Obj : in out Bnd_Node; Elem : Item) is
    begin
-      Assert (Obj.Size < Max_Size,
+      Assert (Obj.Size < Obj.Maximum_Size,
               BC.Overflow'Identity,
               "Insert",
               BSE.Full);
-      Obj.Start := (Obj.Start - 1) mod Max_Size;
+      Obj.Start := ((Obj.Start - 2) mod Obj.Maximum_Size) + 1;
       Obj.Size := Obj.Size + 1;
       Obj.Elems (Obj.Start) := Elem;
    end Insert;
@@ -79,7 +72,7 @@ package body BC.Support.Bounded is
               BC.Range_Error'Identity,
               "Insert",
               BSE.Invalid_Index);
-      Assert (Obj.Size < Max_Size,
+      Assert (Obj.Size < Obj.Maximum_Size,
               BC.Overflow'Identity,
               "Insert",
               BSE.Full);
@@ -92,32 +85,35 @@ package body BC.Support.Bounded is
          --  before the element which the new entry is to be inserted
          --  before (indexed by Actual), 'right' means the part after.
          declare
-            Elems : Elem_Array renames Obj.Elems;
             Start : Elem_Range renames Obj.Start;
             Actual : constant Elem_Range
-              := (Obj.Start + Before - 1) mod Max_Size;
+              := ((Start - 1 + Before - 1) mod Obj.Maximum_Size) + 1;
             Last : constant Elem_Range
-              := (Obj.Start + Obj.Size - 1) mod Max_Size;
+              := ((Start - 1 + Obj.Size - 1) mod Obj.Maximum_Size) + 1;
          begin
-            if Start = 0 or else Start > Actual then
+            if Start = 1 or else Start > Actual then
                --  the left part is wedged, shift the right part up
-               Elems (Actual + 1 .. Last + 1) := Elems (Actual .. Last);
-               Elems (Actual) := Elem;
-            elsif Maximum_Size > 1
-              and then (Last = Elem_Range'Last or else Last < Actual) then
+               Obj.Elems (Actual + 1 .. Last + 1)
+                 := Obj.Elems (Actual .. Last);
+               Obj.Elems (Actual) := Elem;
+            elsif Default_Maximum_Size > 1
+              and then (Last = Obj.Elems'Last or else Last < Actual) then
                --  the right part is wedged, shift the left part down
-               Elems (Start - 1 .. Actual - 2) := Elems (Start .. Actual - 1);
+               Obj.Elems (Start - 1 .. Actual - 2)
+                 := Obj.Elems (Start .. Actual - 1);
                Start := Start - 1;
-               Elems (Actual - 1) := Elem;
-            elsif Maximum_Size > 1 and then Before < Obj.Size / 2 then
+               Obj.Elems (Actual - 1) := Elem;
+            elsif Default_Maximum_Size > 1 and then Before < Obj.Size / 2 then
                --  the left part is shorter, shift it down
-               Elems (Start - 1 .. Actual - 2) := Elems (Start .. Actual - 1);
+               Obj.Elems (Start - 1 .. Actual - 2)
+                 := Obj.Elems (Start .. Actual - 1);
                Start := Start - 1;
-               Elems (Actual - 1) := Elem;
+               Obj.Elems (Actual - 1) := Elem;
             else
                --  the right part is shorter, shift it up
-               Elems (Actual + 1 .. Last + 1) := Elems (Actual .. Last);
-               Elems (Actual) := Elem;
+               Obj.Elems (Actual + 1 .. Last + 1)
+                 := Obj.Elems (Actual .. Last);
+               Obj.Elems (Actual) := Elem;
             end if;
             Obj.Size := Obj.Size + 1;
          end;
@@ -126,12 +122,13 @@ package body BC.Support.Bounded is
 
    procedure Append (Obj : in out Bnd_Node; Elem : Item) is
    begin
-      Assert (Obj.Size < Max_Size,
+      Assert (Obj.Size < Obj.Maximum_Size,
               BC.Overflow'Identity,
               "Append",
               BSE.Full);
       Obj.Size := Obj.Size + 1;
-      Obj.Elems ((Obj.Start + Obj.Size - 1) mod Max_Size) := Elem;
+      Obj.Elems (((Obj.Start - 1 + Obj.Size - 1) mod Obj.Maximum_Size) + 1)
+        := Elem;
    end Append;
 
    procedure Append (Obj : in out Bnd_Node; Elem : Item; After : Positive) is
@@ -140,7 +137,7 @@ package body BC.Support.Bounded is
               BC.Range_Error'Identity,
               "Append",
               BSE.Invalid_Index);
-      Assert (Obj.Size < Max_Size,
+      Assert (Obj.Size < Obj.Maximum_Size,
               BC.Overflow'Identity,
               "Append",
               BSE.Full);
@@ -165,7 +162,7 @@ package body BC.Support.Bounded is
       if Obj.Size = 1 then
          Clear (Obj);
       elsif From = 1 then
-         Obj.Start := (Obj.Start + 1) mod Max_Size;
+         Obj.Start := (Obj.Start mod Obj.Maximum_Size) + 1;
          Obj.Size := Obj.Size - 1;
       elsif From = Obj.Size then
          Obj.Size := Obj.Size - 1;
@@ -176,27 +173,30 @@ package body BC.Support.Bounded is
          --  before the element to be removed (indexed by Actual),
          --  'right' means the part after.
          declare
-            Elems : Elem_Array renames Obj.Elems;
             Start : Elem_Range renames Obj.Start;
             Actual : constant Elem_Range
-              := (Obj.Start + From - 1) mod Max_Size;
+              := ((Start - 1 + From - 1) mod Obj.Maximum_Size) + 1;
             Last : constant Elem_Range
-              := (Obj.Start + Obj.Size - 1) mod Max_Size;
+              := ((Start - 1 + Obj.Size - 1) mod Obj.Maximum_Size) + 1;
          begin
             if Start > Actual then
                --  the left part wraps round; shift the right part down
-               Elems (Actual .. Last - 1) := Elems (Actual + 1 .. Last);
-            elsif Maximum_Size > 1 and then Actual > Last then
+               Obj.Elems (Actual .. Last - 1)
+                 := Obj.Elems (Actual + 1 .. Last);
+            elsif Default_Maximum_Size > 1 and then Actual > Last then
                --  the right part wraps round; shift the left part up
-               Elems (Start + 1 .. Actual) := Elems (Start .. Actual - 1);
+               Obj.Elems (Start + 1 .. Actual)
+                 := Obj.Elems (Start .. Actual - 1);
                Start := Start + 1;
-            elsif Maximum_Size > 1 and then From < Obj.Size / 2 then
+            elsif Obj.Maximum_Size > 1 and then From < Obj.Size / 2 then
                --  the left part is shorter
-               Elems (Start + 1 .. Actual) := Elems (Start .. Actual - 1);
+               Obj.Elems (Start + 1 .. Actual)
+                 := Obj.Elems (Start .. Actual - 1);
                Start := Start + 1;
             else
                --  the right part is shorter
-               Elems (Actual .. Last - 1) := Elems (Actual + 1 .. Last);
+               Obj.Elems (Actual .. Last - 1)
+                 := Obj.Elems (Actual + 1 .. Last);
             end if;
             Obj.Size := Obj.Size - 1;
          end;
@@ -209,12 +209,13 @@ package body BC.Support.Bounded is
               BC.Range_Error'Identity,
               "Replace",
               BSE.Invalid_Index);
-      Obj.Elems ((Obj.Start + Index - 1) mod Max_Size) := Elem;
+      Obj.Elems (((Obj.Start - 1 + Index - 1) mod Obj.Maximum_Size) + 1)
+        := Elem;
    end Replace;
 
    function Available (Obj : Bnd_Node) return Natural is
    begin
-      return Max_Size - Obj.Size;
+      return Obj.Maximum_Size - Obj.Size;
    end Available;
 
    function Length (Obj : Bnd_Node) return Natural is
@@ -237,7 +238,8 @@ package body BC.Support.Bounded is
               BC.Underflow'Identity,
               "Last",
               BSE.Empty);
-      return Obj.Elems ((Obj.Start + Obj.Size - 1) mod Max_Size);
+      return Obj.Elems
+        (((Obj.Start - 1 + Obj.Size - 1) mod Obj.Maximum_Size) + 1);
    end Last;
 
    function Item_At (Obj : Bnd_Node; Index : Positive) return Item is
@@ -246,12 +248,20 @@ package body BC.Support.Bounded is
               BC.Range_Error'Identity,
               "Item_At",
               BSE.Invalid_Index);
-      return Obj.Elems ((Obj.Start + Index - 1) mod Max_Size);
+      return Obj.Elems
+        (((Obj.Start - 1 + Index - 1) mod Obj.Maximum_Size) + 1);
    end Item_At;
 
    function Item_At (Obj : Bnd_Node; Index : Positive) return Item_Ptr is
+      --  We can't take 'Access of components of constant (in parameter)
+      --  objects; but we need to be able to do this so that we can
+      --  return pointers to individual elements. This technique is due
+      --  to Matthew Heaney.
+      subtype This_Elem_Array is Elem_Array (1 .. Obj.Maximum_Size);
+      package Allow_Access
+      is new System.Address_To_Access_Conversions (This_Elem_Array);
       E : Allow_Access.Object_Pointer
-        := Allow_Access.To_Pointer (Obj.Elems'Address);
+        := Allow_Access.To_Pointer (Obj.Elems (Obj.Elems'First)'Address);
    begin
       Assert (Index in 1 .. Obj.Size,
               BC.Range_Error'Identity,
@@ -259,7 +269,8 @@ package body BC.Support.Bounded is
               BSE.Invalid_Index);
       return Item_Ptr
         (Allow_Element_Access.To_Pointer
-         (E ((Obj.Start + Index - 1) mod Max_Size)'Address));
+           (E (((Obj.Start - 1 + Index - 1) mod Obj.Maximum_Size) + 1)
+            'Address));
    end Item_At;
 
    function Location (Obj : Bnd_Node;
@@ -274,7 +285,8 @@ package body BC.Support.Bounded is
               "Start",
               BSE.Invalid_Index);
       for I in Start .. Obj.Size loop
-         if Obj.Elems ((Obj.Start + I - 1) mod Max_Size) = Elem then
+         if Obj.Elems (((Obj.Start - 1 + I - 1) mod Obj.Maximum_Size) + 1)
+           = Elem then
             return I;
          end if;
       end loop;
@@ -284,12 +296,11 @@ package body BC.Support.Bounded is
    procedure Write_Bnd_Node
      (Stream : access Ada.Streams.Root_Stream_Type'Class;
       Obj : Bnd_Node) is
-      Pos : Elem_Range;
    begin
+      Integer'Write (Stream, Obj.Maximum_Size);
       Integer'Write (Stream, Obj.Size);
-      for I in 0 .. Obj.Size - 1 loop
-         Pos := Obj.Start + Elem_Range (I) mod Max_Size;
-         Item'Output (Stream, Obj.Elems (Pos));
+      for I in 1 .. Obj.Size  loop
+         Item'Output (Stream, Item_At (Obj, I));
       end loop;
    end Write_Bnd_Node;
 
@@ -298,15 +309,20 @@ package body BC.Support.Bounded is
       Obj : out Bnd_Node) is
       Count : Integer;
    begin
-      Clear (Obj);
       Integer'Read (Stream, Count);
-      for I in 1 .. Count loop
-         declare
-            Elem : constant Item := Item'Input (Stream);
-         begin
-            Append (Obj, Elem);
-         end;
-      end loop;
+      declare
+         Result : Bnd_Node (Count);
+      begin
+         Integer'Read (Stream, Count);
+         for I in 1 .. Count loop
+            declare
+               Elem : constant Item := Item'Input (Stream);
+            begin
+               Append (Result, Elem);
+            end;
+         end loop;
+         Obj := Result;
+      end;
    end Read_Bnd_Node;
 
 end BC.Support.Bounded;
