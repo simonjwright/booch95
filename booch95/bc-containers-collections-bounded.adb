7d6fb7879abff1978af7911dca18df327f78baec
Simon Wright <simon@pushface.org>
2002-09-09 19:26:24 +0000

    Bounded support uses an unconstrained type.

--- booch95/bc-containers-collections-bounded.adb
+++ booch95/bc-containers-collections-bounded.adb
@@ -1,4 +1,4 @@
---  Copyright (C) 1994-2001 Grady Booch and Simon Wright.
+--  Copyright (C) 1994-2002 Grady Booch and Simon Wright.
 --  All Rights Reserved.
 --
 --      This program is free software; you can redistribute it
@@ -24,94 +24,96 @@ with System.Address_To_Access_Conversions;
 
 package body BC.Containers.Collections.Bounded is
 
-   function "=" (Left, Right : in Collection) return Boolean is
+   function "=" (Left, Right : in Unconstrained_Collection) return Boolean is
       use Collection_Nodes;
    begin
       return Left.Rep = Right.Rep;
    end "=";
 
-   procedure Clear (C : in out Collection) is
+   procedure Clear (C : in out Unconstrained_Collection) is
    begin
       Collection_Nodes.Clear (C.Rep);
    end Clear;
 
-   procedure Insert (C : in out Collection; Elem : Item) is
+   procedure Insert (C : in out Unconstrained_Collection; Elem : Item) is
    begin
       Collection_Nodes.Insert (C.Rep, Elem);
    end Insert;
 
-   procedure Insert (C : in out Collection;
+   procedure Insert (C : in out Unconstrained_Collection;
                      Elem : Item;
                      Before : Positive) is
    begin
       Collection_Nodes.Insert (C.Rep, Elem, Before);
    end Insert;
 
-   procedure Append (C : in out Collection; Elem : Item) is
+   procedure Append (C : in out Unconstrained_Collection; Elem : Item) is
    begin
       Collection_Nodes.Append (C.Rep, Elem);
    end Append;
 
-   procedure Append (C : in out Collection;
+   procedure Append (C : in out Unconstrained_Collection;
                      Elem : Item;
                      After : Positive) is
    begin
       Collection_Nodes.Append (C.Rep, Elem, After);
    end Append;
 
-   procedure Remove (C : in out Collection; At_Index : Positive) is
+   procedure Remove (C : in out Unconstrained_Collection;
+                     At_Index : Positive) is
    begin
       Collection_Nodes.Remove (C.Rep, At_Index);
    end Remove;
 
-   procedure Replace (C : in out Collection;
+   procedure Replace (C : in out Unconstrained_Collection;
                       At_Index : Positive;
                       Elem : Item) is
    begin
       Collection_Nodes.Replace (C.Rep, At_Index, Elem);
    end Replace;
 
-   function Available (C : in Collection) return Natural is
+   function Available (C : in Unconstrained_Collection) return Natural is
    begin
       return Collection_Nodes.Available (C.Rep);
    end Available;
 
-   function Length (C : Collection) return Natural is
+   function Length (C : Unconstrained_Collection) return Natural is
    begin
       return Collection_Nodes.Length (C.Rep);
    end Length;
 
-   function Is_Empty (C : Collection) return Boolean is
+   function Is_Empty (C : Unconstrained_Collection) return Boolean is
    begin
       return Collection_Nodes.Length (C.Rep) = 0;
    end Is_Empty;
 
-   function First (C : Collection) return Item is
+   function First (C : Unconstrained_Collection) return Item is
    begin
       return Collection_Nodes.First (C.Rep);
    end First;
 
-   function Last (C : Collection) return Item is
+   function Last (C : Unconstrained_Collection) return Item is
    begin
       return Collection_Nodes.Last (C.Rep);
    end Last;
 
    function Item_At
-     (C : Collection; At_Index : Positive) return Item is
+     (C : Unconstrained_Collection; At_Index : Positive) return Item is
    begin
       return Item_At (C, At_Index).all;
    end Item_At;
 
-   function Location (C : Collection; Elem : Item) return Natural is
+   function Location (C : Unconstrained_Collection;
+                      Elem : Item) return Natural is
    begin
       return Collection_Nodes.Location (C.Rep, Elem);
    end Location;
 
    package Address_Conversions
-   is new System.Address_To_Access_Conversions (Collection);
+   is new System.Address_To_Access_Conversions (Unconstrained_Collection);
 
    function New_Iterator
-     (For_The_Collection : Collection) return Iterator'Class is
+     (For_The_Collection : Unconstrained_Collection) return Iterator'Class is
       Result : Collection_Iterator;
    begin
       Result.For_The_Container :=
@@ -121,7 +123,7 @@ package body BC.Containers.Collections.Bounded is
    end New_Iterator;
 
    function Item_At
-     (C : Collection; Index : Positive) return Item_Ptr is
+     (C : Unconstrained_Collection; Index : Positive) return Item_Ptr is
    begin
       return Collection_Nodes.Item_At (C.Rep, Index);
    end Item_At;
@@ -129,7 +131,7 @@ package body BC.Containers.Collections.Bounded is
    Empty_Container : Collection;
    pragma Warnings (Off, Empty_Container);
 
-   function Null_Container return Collection is
+   function Null_Container return Unconstrained_Collection is
    begin
       return Empty_Container;
    end Null_Container;
