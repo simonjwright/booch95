16a981bc15709f9875d9dbf1e1f17717696e0e79
Simon Wright <simon@pushface.org>
2002-09-27 20:54:09 +0000

    Added comments.

--- booch95/bc-support-memory_streams.ads
+++ booch95/bc-support-memory_streams.ads
@@ -27,20 +27,30 @@ package BC.Support.Memory_Streams is
    type Stream_Type
      (Capacity : Ada.Streams.Stream_Element_Count)
       is new Ada.Streams.Root_Stream_Type with private;
+   --  Provides an in-memory Stream.
 
    procedure Read
      (Stream : in out Stream_Type;
       Item   : out Ada.Streams.Stream_Element_Array;
       Last   : out Ada.Streams.Stream_Element_Offset);
+   --  Removes Item'Length storage elements (or, as many as remain)
+   --  from Stream. Last is updated to the final index in Item that
+   --  was updated (normally, Item'Last). When Stream was already
+   --  empty, Item will be unchanged and Last will be set to
+   --  Item'First.
 
    procedure Write
      (Stream : in out Stream_Type;
       Item   : in Ada.Streams.Stream_Element_Array);
+   --  Adds Item to Stream. Raises Ada.IO_Exceptions.End_Error on
+   --  overrun.
 
    function Contents (Stream : Stream_Type)
                      return Ada.Streams.Stream_Element_Array;
+   --  Returns a copy of the contents of Stream.
 
    procedure Reset (Stream : out Stream_Type);
+   --  Clears Stream.
 
 private
 
