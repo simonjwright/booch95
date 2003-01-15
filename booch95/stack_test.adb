6f31904c75e9b4c266504632fbaedded2fc01169
Simon Wright <simon@pushface.org>
2003-01-15 05:59:32 +0000

    Copyright date update.

--- booch95/stack_test.adb
+++ booch95/stack_test.adb
@@ -1,6 +1,6 @@
 --  Copyright 1994 Grady Booch
 --  Copyright 1994-1997 David Weller
---  Copyright 1998-2002 Simon Wright <simon@pushface.org>
+--  Copyright 1998-2003 Simon Wright <simon@pushface.org>
 
 --  This package is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
