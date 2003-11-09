a6d7d7844b5790475a56c3b068326c273ca3bbfd
simon <simon@localhost>
2003-11-09 12:42:32 +0000

    Removed Assert.

--- booch95/bc-support-exceptions.adb
+++ booch95/bc-support-exceptions.adb
@@ -28,53 +28,6 @@
 package body BC.Support.Exceptions is
 
 
-   function Reason_Message (For_The_Reason : Reason) return String;
-
-   function Reason_Message (For_The_Reason : Reason) return String is
-   begin
-      case For_The_Reason is
-         when No_Reason_Given => return "";
-         when Disjoint => return "objects are members of different structures";
-         when Duplicate => return "object already exists";
-         when Empty => return "object is empty";
-         when Full => return "object is full";
-         when Illegal => return "illegal pattern";
-         when Invalid_Index => return "index is out of range";
-         when Invalid_Number => return "string does not denote a valid number";
-         when Missing => return "object does not exist";
-         when Not_Empty => return "object is not empty";
-         when Not_Root => return "object is not at root of structure";
-         when Is_Null => return "object is null";
-         when Out_Of_Memory => return "storage requested not available";
-         when Referenced =>
-            return "object is referenced and cannot be destroyed";
-         when Timing => return "possible race condition";
-         when Too_Large => return "object is too large";
-         when Too_Small => return "object is too small";
-      end case;
-   end Reason_Message;
-
-
-   procedure Assert (Condition : Boolean;
-                     Raising_If_False : Ada.Exceptions.Exception_Id;
-                     From_Subprogram : String;
-                     With_Reason : Reason := No_Reason_Given) is
-   begin
-      if not Condition then
-         if With_Reason = No_Reason_Given then
-            Ada.Exceptions.Raise_Exception
-              (Raising_If_False,
-               Module & "." & From_Subprogram);
-         else
-            Ada.Exceptions.Raise_Exception
-              (Raising_If_False,
-               Module & "." & From_Subprogram & ": "
-               & Reason_Message (With_Reason));
-         end if;
-      end if;
-   end Assert;
-
-
    procedure Report
      (The_Exception : Ada.Exceptions.Exception_Occurrence;
       To : Ada.Text_IO.File_Type := Ada.Text_IO.Standard_Output) is
