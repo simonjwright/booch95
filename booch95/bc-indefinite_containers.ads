d27d4cc20d217347a431b3aae17ba4329a998055
krischik <krischik@localhost>
2005-10-17 17:04:37 +0000

    Change the marker

--- booch95/bc-indefinite_containers.ads
+++ booch95/bc-indefinite_containers.ads
@@ -2,6 +2,7 @@
 --  Copyright 2005 Martin Krischik
 --  Copyright 1994-1997 David Weller
 --  Copyright 1998-2003 Simon Wright <simon@pushface.org>
+--  Copyright 2005 Martin Krischik
 
 --  This package is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
