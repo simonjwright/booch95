8f1f0e3101a9ca68e95c82332500915f808c8933
Simon Wright <simon@pushface.org>
2006-12-25 12:02:22 +0000

    Rotate moved to abstract Ring.

--- booch95/bc-containers-rings-dynamic.ads
+++ booch95/bc-containers-rings-dynamic.ads
@@ -1,5 +1,5 @@
 --  Copyright 1994 Grady Booch
---  Copyright 1998-2002 Simon Wright <simon@pushface.org>
+--  Copyright 1998-2006 Simon Wright <simon@pushface.org>
 
 --  This package is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
@@ -58,11 +58,6 @@ package BC.Containers.Rings.Dynamic is
    --  was previously one place forward from the top.
    --  If the removed item was the marked item, the mark now
    --  designates the new top.
-
-   procedure Rotate (R : in out Ring; Dir : Direction := Forward);
-   --  Rotate the top of the ring in the given direction. The ring's
-   --  mark is unaffected. If there is exactly one item in the ring,
-   --  rotating either direction always returns to the same item.
 
    function Extent (R : Ring) return Natural;
    --  Return the number of items in the ring.
