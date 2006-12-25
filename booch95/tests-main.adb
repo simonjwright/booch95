41eba2db81b9257f586b8b77bdd45b8cb93f45ff
simonjwright <simonjwright@localhost>
2006-12-25 12:04:20 +0000

    Added Auto Pointers and Rings.

--- booch95/tests-main.adb
+++ booch95/tests-main.adb
@@ -3,10 +3,25 @@
 --  Tests for the Booch Components.
 
 with AUnit.Test_Runner;
+with AUnit.Test_Suites;
+with Tests.Auto_Pointers;
 with Tests.Multiway_Trees;
+with Tests.Rings;
 
 procedure Tests.Main is
-   procedure Run is new AUnit.Test_Runner (Tests.Multiway_Trees.Suite);
+
+   function Suites return AUnit.Test_Suites.Access_Test_Suite;
+   procedure Run is new AUnit.Test_Runner (Suites);
+   function Suites return AUnit.Test_Suites.Access_Test_Suite is
+      Result : constant AUnit.Test_Suites.Access_Test_Suite
+        := new AUnit.Test_Suites.Test_Suite;
+   begin
+      AUnit.Test_Suites.Add_Test (Result, Auto_Pointers.Suite);
+      AUnit.Test_Suites.Add_Test (Result, Multiway_Trees.Suite);
+      AUnit.Test_Suites.Add_Test (Result, Rings.Suite);
+      return Result;
+   end Suites;
+
 begin
    Run;
 end Tests.Main;
