d2de39ca5028f01199b36dadf03130cf30e02aec
simonjwright <simonjwright@localhost>
2007-05-28 15:42:33 +0000

    Continues; third test case added (2 elements).

--- booch95/tests-rings.adb
+++ booch95/tests-rings.adb
@@ -19,10 +19,12 @@ pragma Warnings (Off, Ada.Text_IO);
 
 package body Tests.Rings is
 
-   package Containers is new BC.Containers (Item => Character);
+   package Abstract_Containers is new BC.Containers (Item => Character);
 
-   package Abstract_Rings is new Containers.Rings;
+   package Abstract_Rings is new Abstract_Containers.Rings;
    use Abstract_Rings;
+
+   function Value (R : Abstract_Rings.Abstract_Ring'Class) return String;
 
 
    generic
@@ -38,17 +40,23 @@ package body Tests.Rings is
       function Name (C : Case_1) return String_Access;
       procedure Register_Tests (C : in out Case_1);
 
-      type Case_2 is new Test_Case with record
-         R : Ring;
-      end record;
+      type Case_2 is new Test_Case with null record;
       function Name (C : Case_2) return String_Access;
       procedure Register_Tests (C : in out Case_2);
       procedure Set_Up (C : in out Case_2);
       procedure Tear_Down (C : in out Case_2);
 
+      type Case_3 is new Test_Case with null record;
+      function Name (C : Case_3) return String_Access;
+      procedure Register_Tests (C : in out Case_3);
+      procedure Set_Up (C : in out Case_3);
+      procedure Tear_Down (C : in out Case_3);
+
    end Test_G;
 
    package body Test_G is
+
+      R : Ring;
 
       -----------------------
       --  Test procedures  --
@@ -57,8 +65,6 @@ package body Tests.Rings is
       procedure Extent_Initially_Zero (C : in out Test_Case'Class);
       procedure Extent_Initially_Zero (C : in out Test_Case'Class) is
          pragma Warnings (Off, C);
-         R : Ring;
-         pragma Warnings (Off, R);
       begin
          Assert (Extent (R) = 0,
                  "Extent should be 0");
@@ -67,8 +73,6 @@ package body Tests.Rings is
       procedure Initially_Empty (C : in out Test_Case'Class);
       procedure Initially_Empty (C : in out Test_Case'Class) is
          pragma Warnings (Off, C);
-         R : Ring;
-         pragma Warnings (Off, R);
       begin
          Assert (Is_Empty (R),
                  "should be empty");
@@ -77,8 +81,6 @@ package body Tests.Rings is
       procedure Initially_At_Mark (C : in out Test_Case'Class);
       procedure Initially_At_Mark (C : in out Test_Case'Class) is
          pragma Warnings (Off, C);
-         R : Ring;
-         pragma Warnings (Off, R);
       begin
          Assert (At_Mark (R),
                  "should be at mark");
@@ -87,8 +89,6 @@ package body Tests.Rings is
       procedure Initially_No_Content (C : in out Test_Case'Class);
       procedure Initially_No_Content (C : in out Test_Case'Class) is
          pragma Warnings (Off, C);
-         R : Ring;
-         pragma Warnings (Off, R);
       begin
          declare
             C : Character;
@@ -100,6 +100,23 @@ package body Tests.Rings is
             when BC.Underflow => null;
          end;
       end Initially_No_Content;
+
+      procedure Value_Initially_Empty (C : in out Test_Case'Class);
+      procedure Value_Initially_Empty (C : in out Test_Case'Class) is
+         pragma Warnings (Off, C);
+      begin
+         Assert (Value (R) = "",
+                 "should be empty");
+      end Value_Initially_Empty;
+
+      procedure Check_Clear (C : in out Test_Case'Class);
+      procedure Check_Clear (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Clear (R);
+         Assert (Is_Empty (R),
+                 "ring isn't empty");
+      end Check_Clear;
 
       function Name (C : Case_1) return String_Access is
          pragma Warnings (Off, C);
@@ -125,6 +142,14 @@ package body Tests.Rings is
            (C,
             Initially_No_Content'Unrestricted_Access,
             "initialized ring has no content");
+         Register_Routine
+           (C,
+            Value_Initially_Empty'Unrestricted_Access,
+            "value of initialized ring is empty string");
+         Register_Routine
+           (C,
+            Check_Clear'Unrestricted_Access,
+            "empty ring can be cleared");
       end Register_Tests;
 
 
@@ -134,7 +159,7 @@ package body Tests.Rings is
 
       procedure Extent_Is_One (C : in out Test_Case'Class);
       procedure Extent_Is_One (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
+         pragma Unreferenced (C);
       begin
          Assert (Extent (R) = 1,
                  "ring's extent is not 1");
@@ -142,7 +167,7 @@ package body Tests.Rings is
 
       procedure Is_Not_Empty (C : in out Test_Case'Class);
       procedure Is_Not_Empty (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
+         pragma Unreferenced (C);
       begin
          Assert (not Is_Empty (R),
                  "shouldn't be empty");
@@ -150,7 +175,7 @@ package body Tests.Rings is
 
       procedure Is_At_Mark (C : in out Test_Case'Class);
       procedure Is_At_Mark (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
+         pragma Unreferenced (C);
       begin
          Assert (At_Mark (R),
                  "should be at mark");
@@ -158,24 +183,15 @@ package body Tests.Rings is
 
       procedure Check_Top_After_Single_Insert (C : in out Test_Case'Class);
       procedure Check_Top_After_Single_Insert (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
+         pragma Unreferenced (C);
       begin
          Assert (Top (R) = 'a',
                  "has wrong top");
       end Check_Top_After_Single_Insert;
 
-      procedure Check_Clear_On_Single_Entry (C : in out Test_Case'Class);
-      procedure Check_Clear_On_Single_Entry (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
-      begin
-         Clear (R);
-         Assert (Is_Empty (R),
-                 "ring isn't empty");
-      end Check_Clear_On_Single_Entry;
-
       procedure Check_Empty_After_Pop (C : in out Test_Case'Class);
       procedure Check_Empty_After_Pop (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
+         pragma Unreferenced (C);
       begin
          Pop (R);
          Assert (Is_Empty (R),
@@ -184,11 +200,9 @@ package body Tests.Rings is
 
       procedure Check_Rotating_Single_Entry (C : in out Test_Case'Class);
       procedure Check_Rotating_Single_Entry (C : in out Test_Case'Class) is
-         R : Ring renames Case_2 (C).R;
+         pragma Unreferenced (C);
       begin
          Rotate (R);
-         Assert (Extent (R) = 1,
-                 "ring's extent is not 1");
          Assert (Extent (R) = 1,
                  "ring's extent is not 1");
          Assert (At_Mark (R),
@@ -198,13 +212,19 @@ package body Tests.Rings is
          Rotate (R, Dir => Backward);
          Assert (Extent (R) = 1,
                  "ring's extent is not 1");
-         Assert (Extent (R) = 1,
-                 "ring's extent is not 1");
          Assert (At_Mark (R),
                  "should be at mark");
          Assert (Top (R) = 'a',
                  "has wrong top");
       end Check_Rotating_Single_Entry;
+
+      procedure Check_Value_After_Single_Insert (C : in out Test_Case'Class);
+      procedure Check_Value_After_Single_Insert (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Assert (Value (R) = "a",
+                 "has wrong value");
+      end Check_Value_After_Single_Insert;
 
       --        procedure  (C : in out Test_Case'Class);
 --        procedure  (C : in out Test_Case'Class) is
@@ -248,7 +268,7 @@ package body Tests.Rings is
             "check top");
          Register_Routine
            (C,
-            Check_Clear_On_Single_Entry'Unrestricted_Access,
+            Check_Clear'Unrestricted_Access,
             "clearing single entry");
          Register_Routine
            (C,
@@ -258,6 +278,10 @@ package body Tests.Rings is
            (C,
             Check_Rotating_Single_Entry'Unrestricted_Access,
             "rotating single entry");
+         Register_Routine
+           (C,
+            Check_Value_After_Single_Insert'Unrestricted_Access,
+            "check value");
          --           Register_Routine
 --             (C,
 --              'Unrestricted_Access,
@@ -265,13 +289,155 @@ package body Tests.Rings is
       end Register_Tests;
 
       procedure Set_Up (C : in out Case_2) is
+         pragma Unreferenced (C);
       begin
-         Insert (C.R, 'a');
+         Insert (R, 'a');
       end Set_Up;
 
       procedure Tear_Down (C : in out Case_2) is
+         pragma Unreferenced (C);
       begin
-         Clear (C.R);
+         Clear (R);
+      end Tear_Down;
+
+      --------------
+      --  Case_3  --
+      --------------
+
+      procedure Extent_Is_Two (C : in out Test_Case'Class);
+      procedure Extent_Is_Two (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Assert (Extent (R) = 2,
+                 "ring's extent is not 2");
+      end Extent_Is_Two;
+
+      procedure Not_At_Mark (C : in out Test_Case'Class);
+      procedure Not_At_Mark (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Assert (not At_Mark (R),
+                 "shouldn't be at mark");
+      end Not_At_Mark;
+
+      procedure Check_Top_After_Double_Insert (C : in out Test_Case'Class);
+      procedure Check_Top_After_Double_Insert (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Assert (Top (R) = 'b',
+                 "has wrong top");
+      end Check_Top_After_Double_Insert;
+
+      procedure Check_Value_After_Pop (C : in out Test_Case'Class);
+      procedure Check_Value_After_Pop (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Pop (R);
+         Assert (Value (R) = "a",
+                 "has wrong value");
+      end Check_Value_After_Pop;
+
+      procedure Check_Rotating_Double_Entry (C : in out Test_Case'Class);
+      procedure Check_Rotating_Double_Entry (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Rotate (R);
+         Assert (Value (R) = "ab",
+                 "has wrong value after rotation");
+         Assert (At_Mark (R),
+                 "should be at mark");
+         Assert (Top (R) = 'a',
+                 "has wrong top");
+         Rotate (R, Dir => Backward);
+         Assert (Value (R) = "ba",
+                 "has wrong value after rotation backward");
+         Assert (not At_Mark (R),
+                 "shouldn't be at mark");
+         Assert (Top (R) = 'b',
+                 "has wrong top");
+      end Check_Rotating_Double_Entry;
+
+      procedure Check_Value_Of_Double_Entry (C : in out Test_Case'Class);
+      procedure Check_Value_Of_Double_Entry (C : in out Test_Case'Class) is
+         pragma Unreferenced (C);
+      begin
+         Assert (Value (R) = "ba",
+                 "has wrong value");
+      end Check_Value_Of_Double_Entry;
+
+      --        procedure  (C : in out Test_Case'Class);
+--        procedure  (C : in out Test_Case'Class) is
+--       R : Ring renames case_3 (C).R;
+--        begin
+--       Assert (Extent (R) = 1,
+--               "ring's extent is not 1");
+--        end ;
+
+--        procedure  (C : in out Test_Case'Class);
+--        procedure  (C : in out Test_Case'Class) is
+--       R : Ring renames case_3 (C).R;
+--        begin
+--       Assert (Extent (R) = 1,
+--               "ring's extent is not 1");
+--        end ;
+
+      function Name (C : Case_3) return String_Access is
+         pragma Warnings (Off, C);
+      begin
+         return new String'("Rings (double entry) -- " & Test_Name);
+      end Name;
+
+      procedure Register_Tests (C : in out Case_3) is
+      begin
+         Register_Routine
+           (C,
+            Extent_Is_Two'Unrestricted_Access,
+            "extent is 2");
+         Register_Routine
+           (C,
+            Not_At_Mark'Unrestricted_Access,
+            "ring isn't at mark");
+         Register_Routine
+           (C,
+            Is_Not_Empty'Unrestricted_Access,
+            "ring isn't empty");
+         Register_Routine
+           (C,
+            Check_Top_After_Double_Insert'Unrestricted_Access,
+            "check top");
+         Register_Routine
+           (C,
+            Check_Value_After_Pop'Unrestricted_Access,
+            "checking pop of double entry");
+         Register_Routine
+           (C,
+            Check_Clear'Unrestricted_Access,
+            "clearing double entry");
+         Register_Routine
+           (C,
+            Check_Rotating_Double_Entry'Unrestricted_Access,
+            "rotating double entry");
+         Register_Routine
+           (C,
+            Check_Value_Of_Double_Entry'Unrestricted_Access,
+            "checking initial value of double entry");
+         --           Register_Routine
+--             (C,
+--              'Unrestricted_Access,
+--              "");
+      end Register_Tests;
+
+      procedure Set_Up (C : in out Case_3) is
+         pragma Unreferenced (C);
+      begin
+         Insert (R, 'a');
+         Insert (R, 'b');
+      end Set_Up;
+
+      procedure Tear_Down (C : in out Case_3) is
+         pragma Unreferenced (C);
+      begin
+         Clear (R);
       end Tear_Down;
 
       function Suite return AUnit.Test_Suites.Access_Test_Suite is
@@ -280,6 +446,7 @@ package body Tests.Rings is
       begin
          AUnit.Test_Suites.Add_Test (Result, new Case_1);
          AUnit.Test_Suites.Add_Test (Result, new Case_2);
+         AUnit.Test_Suites.Add_Test (Result, new Case_3);
          return Result;
       end Suite;
 
@@ -308,6 +475,23 @@ package body Tests.Rings is
       AUnit.Test_Suites.Add_Test (Result, Unmanaged_Tests.Suite);
       return Result;
    end Suite;
+
+
+   function Value (R : Abstract_Rings.Abstract_Ring'Class) return String is
+      Result : String (1 .. Extent (R));
+      Pos : Positive := 1;
+      It : Abstract_Containers.Iterator'Class := New_Iterator (R);
+      use Abstract_Containers;
+   begin
+      while not Is_Done (It) loop
+         Result (Pos) := Current_Item (It);
+         Pos := Pos + 1;
+         Next (It);
+      end loop;
+      Assert (Pos = Result'Last + 1,
+              "iteration terminated early in Value");
+      return Result;
+   end Value;
 
 
 end Tests.Rings;
