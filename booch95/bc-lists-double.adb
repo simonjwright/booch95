4706016b5f50d990b3a6d8db735c443970e82069
simonjwright <simonjwright@localhost>
2007-08-18 17:44:21 +0000

    Correct copyright notice.

--- booch95/bc-lists-double.adb
+++ booch95/bc-lists-double.adb
@@ -1,6 +1,6 @@
 --  Copyright 1994 Grady Booch
 --  Copyright 1994-1997 David Weller
---  Copyright 1998-2004 Simon Wright <simon@pushface.org>
+--  Copyright 1998-2007 Simon Wright <simon@pushface.org>
 
 --  This package is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
