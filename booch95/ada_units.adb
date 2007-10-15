6072520f2580bfdb93dc385b090d4a986d3acaf5
krischik <krischik@localhost>
2007-10-15 17:20:48 +0000

    Damm CVS deleting all those files. I which we had swiched to SVN

---
+++ booch95/ada_units.adb
@@ -0,0 +1,40 @@
+--  Copyright 1998-2002 Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  $Id$
+
+with Ada.Text_IO;
+with Ada_Unit_Support;
+
+procedure Ada_Units is
+   use Ada_Unit_Support;
+   U_Text_IO : Unit_P := Create_Normal_Unit ("ada.text_io");
+   U_Ada_Units : Unit_P := Create_Normal_Unit ("ada_units");
+   U_Ada_Unit_Support : Unit_P := Create_Normal_Unit ("ada_unit_support");
+   U_Directed : Unit_P := Create_Generic_Unit ("bc.graphs.directed");
+   U_Undirected : Unit_P := Create_Generic_Unit ("bc.graphs.undirected");
+   U_Graphs : Unit_P := Create_Generic_Unit ("bc.graphs");
+begin
+   Add_Dependency (U_Graphs, U_Ada_Unit_Support);
+   Add_Dependency (U_Directed, U_Ada_Unit_Support);
+   Add_Dependency (U_Undirected, U_Ada_Unit_Support);
+   Add_Dependency (U_Text_IO, U_Ada_Units);
+   Add_Dependency (U_Ada_Unit_Support, U_Ada_Units);
+   Add_Dependency (U_Text_IO, U_Ada_Unit_Support);
+   Report_Dependencies;
+   Ada.Text_IO.New_Line;
+   Report_Dependencies (U_Ada_Units);
+   Ada.Text_IO.New_Line;
+   Report_Dependencies (U_Ada_Unit_Support);
+end Ada_Units;
