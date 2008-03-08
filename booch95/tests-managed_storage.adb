b4aeb89b67161db84149f96c24fb67c6cddbfcc8
simonjwright <simonjwright@localhost>
2008-03-08 18:06:26 +0000

    Added Check_Chunks helper. Moved the P128 pool declarations into the test procedure bodies.

--- booch95/tests-managed_storage.adb
+++ booch95/tests-managed_storage.adb
@@ -17,15 +17,44 @@ with System.Storage_Elements;
 
 package body Tests.Managed_Storage is
 
-
    package MS renames BC.Support.Managed_Storage;
-   P128 : MS.Pool (128);
-
    package SSE renames System.Storage_Elements;
 
    type Case_1 is new Test_Case with null record;
    function Name (C : Case_1) return String_Access;
    procedure Register_Tests (C : in out Case_1);
+
+   procedure Check_Chunks
+     (In_Pool : MS.Pool;
+      Check : Positive;
+      Expected_Total : Natural;
+      Expected_Dirty : Natural;
+      Expected_Unused : Natural);
+   procedure Check_Chunks
+     (In_Pool : MS.Pool;
+      Check : Positive;
+      Expected_Total : Natural;
+      Expected_Dirty : Natural;
+      Expected_Unused : Natural)
+   is
+   begin
+      Assert (MS.Total_Chunks (In_Pool) = Expected_Total
+                and MS.Dirty_Chunks (In_Pool) = Expected_Dirty
+                and MS.Unused_Chunks (In_Pool) = Expected_Unused,
+              "check" & Check'Img
+                & ": expecting t:"
+                & Expected_Total'Img
+                & " d:"
+                & Expected_Dirty'Img
+                & " u:"
+                & Expected_Unused'Img
+                & ", got t:"
+                & MS.Total_Chunks (In_Pool)'Img
+                & " d:"
+                & MS.Dirty_Chunks (In_Pool)'Img
+                & " u:"
+                & MS.Unused_Chunks (In_Pool)'Img);
+   end Check_Chunks;
 
 
    -----------------------
@@ -35,56 +64,33 @@ package body Tests.Managed_Storage is
    procedure Allocation (C : in out Test_Case'Class);
    procedure Allocation (C : in out Test_Case'Class) is
       pragma Warnings (Off, C);
+      P128 : MS.Pool (128);
       type String_P is access String;
       for String_P'Storage_Pool use P128;
       procedure Free is new Ada.Unchecked_Deallocation (String, String_P);
       P : String_P;
    begin
       P := new String (1 .. 32);
-      Assert (MS.Total_Chunks (P128) = 1,
-              "wrong number of chunks");
-      Assert (MS.Dirty_Chunks (P128) = 1,
-              "wrong number of dirty chunks");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks");
+      Check_Chunks (P128, 1, 1, 1, 0);
       Free (P);
       --  The maximum length string is the pool size less two integers
       --  for the bounds of the unconstrained string.
       P := new String
         (1 .. 128 - 2 * Integer'Max_Size_In_Storage_Elements);
-      Assert (MS.Total_Chunks (P128) = 2,
-              "wrong number of chunks (2)");
-      Assert (MS.Dirty_Chunks (P128) = 2,
-              "wrong number of dirty chunks (2)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (2)");
+      Check_Chunks (P128, 2, 2, 2, 0);
       Free (P);
-      Assert (MS.Total_Chunks (P128) = 2,
-              "wrong number of chunks (3)");
-      Assert (MS.Dirty_Chunks (P128) = 2,
-              "wrong number of dirty chunks (3)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (3)");
+      Check_Chunks (P128, 2, 2, 2, 0);
       MS.Reclaim_Unused_Chunks (P128);
-      Assert (MS.Total_Chunks (P128) = 2,
-              "wrong number of chunks (4)");
-      Assert (MS.Dirty_Chunks (P128) = 0,
-              "wrong number of dirty chunks (4)");
-      Assert (MS.Unused_Chunks (P128) = 2,
-              "wrong number of unused chunks (4)");
+      Check_Chunks (P128, 3, 2, 0, 2);
       MS.Purge_Unused_Chunks (P128);
-      Assert (MS.Total_Chunks (P128) = 0,
-              "wrong number of chunks (5)");
-      Assert (MS.Dirty_Chunks (P128) = 0,
-              "wrong number of dirty chunks (5)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (5)");
+      Check_Chunks (P128, 4, 0, 0, 0);
    end Allocation;
 
 
    procedure Zero_Allocation (C : in out Test_Case'Class);
    procedure Zero_Allocation (C : in out Test_Case'Class) is
       pragma Warnings (Off, C);
+      P128 : MS.Pool (128);
       subtype Empty is String (1 .. 0);
       type Empty_P is access Empty;
       for Empty_P'Storage_Pool use P128;
@@ -92,48 +98,18 @@ package body Tests.Managed_Storage is
       P, Q : Empty_P;
    begin
       P := new Empty;
-      Assert (MS.Total_Chunks (P128) = 1,
-              "wrong number of chunks (1)");
-      Assert (MS.Dirty_Chunks (P128) = 1,
-              "wrong number of dirty chunks (1)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (1)");
+      Check_Chunks (P128, 1, 1, 1, 0);
       Q := new Empty;
-      Assert (MS.Total_Chunks (P128) = 1,
-              "wrong number of chunks (2)");
-      Assert (MS.Dirty_Chunks (P128) = 1,
-              "wrong number of dirty chunks (2)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (2)");
+      Check_Chunks (P128, 2, 1, 1, 0);
       Free (P);
-      Assert (MS.Total_Chunks (P128) = 1,
-              "wrong number of chunks (3)");
-      Assert (MS.Dirty_Chunks (P128) = 1,
-              "wrong number of dirty chunks (3)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (3)");
+      Check_Chunks (P128, 3, 1, 1, 0);
       MS.Reclaim_Unused_Chunks (P128);
-      Assert (MS.Total_Chunks (P128) = 1,
-              "wrong number of chunks (4)");
-      Assert (MS.Dirty_Chunks (P128) = 1,
-              "wrong number of dirty chunks (4)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (4)");
+      Check_Chunks (P128, 4, 1, 1, 0);
       Free (Q);
       MS.Reclaim_Unused_Chunks (P128);
-      Assert (MS.Total_Chunks (P128) = 1,
-              "wrong number of chunks (5)");
-      Assert (MS.Dirty_Chunks (P128) = 0,
-              "wrong number of dirty chunks (5)");
-      Assert (MS.Unused_Chunks (P128) = 1,
-              "wrong number of unused chunks (5)");
+      Check_Chunks (P128, 5, 1, 0, 1);
       MS.Purge_Unused_Chunks (P128);
-      Assert (MS.Total_Chunks (P128) = 0,
-              "wrong number of chunks (6)");
-      Assert (MS.Dirty_Chunks (P128) = 0,
-              "wrong number of dirty chunks (6)");
-      Assert (MS.Unused_Chunks (P128) = 0,
-              "wrong number of unused chunks (6)");
+      Check_Chunks (P128, 6, 0, 0, 0);
    end Zero_Allocation;
 
 
