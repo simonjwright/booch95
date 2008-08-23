eedf828e0339416c3674620e42ffc29bfb904246
simonjwright <simonjwright@localhost>
2008-08-23 11:48:56 +0000

    Sorted (c) lines. Layout change.

--- booch95/bc-indefinite_containers-queues.adb
+++ booch95/bc-indefinite_containers-queues.adb
@@ -1,7 +1,7 @@
 --  Copyright 1994 Grady Booch
---  Copyright 2005 Martin Krischik
 --  Copyright 1994-1997 David Weller
---  Copyright 1998-2002 Simon Wright <simon@pushface.org>
+--  Copyright 1998-2008 Simon Wright <simon@pushface.org>
+--  Copyright 2005 Martin Krischik
 
 --  This package is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
@@ -80,7 +80,8 @@ package body BC.Indefinite_Containers.Queues is
       begin
          while not Is_Done (Left_Iter) and then
            not Is_Done (Right_Iter) loop
-            if Item (Current_Item (Left_Iter)) /= Item (Current_Item (Right_Iter)) then
+            if Item (Current_Item (Left_Iter))
+              /= Item (Current_Item (Right_Iter)) then
                return False;
             end if;
             Next (Left_Iter);
